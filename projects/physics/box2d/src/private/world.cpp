a559982dbb51e14faba177af8c38e7c6d7a03ecb
Stephanie Hurlburt <stephanie.hurlburt@downstream.com>
2013-09-12 09:13:30 -0700

    Modified some physics joint settings.

--- projects/physics/box2d/src/private/world.cpp
+++ projects/physics/box2d/src/private/world.cpp
@@ -96,18 +96,18 @@ void World::createDistanceJoint(const SpriteBody& body1, const SpriteBody& body2
 		//jointDef.dampingRatio = 50.0f;
 		//jointDef.frequencyHz = 10.0f;
 		// Trying to find settings that work well with colliding bodies:
-		jointDef.dampingRatio = 0.95f;
-		jointDef.frequencyHz = 2.0f;
-		// Trying out yet more settings for colliding bodies. The previous ones were alright, but not perfect:
-		//jointDef.dampingRatio = 0.2f;
+		//jointDef.dampingRatio = 0.95f;
 		//jointDef.frequencyHz = 2.0f;
-		jointDef.collideConnected = false;
+		// Trying out yet more settings for colliding bodies. The previous ones were alright, but not perfect:
+		jointDef.dampingRatio = 0.7f;
+		jointDef.frequencyHz = 1.5f;
+		jointDef.collideConnected = true;
 		jointDef.length = getCi2BoxScale()*(length);
 		b2DistanceJoint* joint = (b2DistanceJoint*) mWorld->CreateJoint(&jointDef);
 
-		std::cout << "JOINT ANCHORS: " << std::endl;
-		std::cout << "A " << joint->GetAnchorA().x << " " << joint->GetAnchorA().y << std::endl;
-		std::cout << "B " << joint->GetAnchorB().x << " " << joint->GetAnchorB().y << std::endl;
+		//std::cout << "JOINT ANCHORS: " << std::endl;
+		//std::cout << "A " << joint->GetAnchorA().x << " " << joint->GetAnchorA().y << std::endl;
+		//std::cout << "B " << joint->GetAnchorB().x << " " << joint->GetAnchorB().y << std::endl;
 
 		mDistanceJoints.insert(mDistanceJoints.end(), joint);
 	}
@@ -118,7 +118,7 @@ void World::resizeDistanceJoint(const SpriteBody& body1, const SpriteBody& body2
 		b2DistanceJoint* joint  = *it;
 		if (joint->GetBodyA() == body1.mBody && joint->GetBodyB() == body2.mBody
 			|| joint->GetBodyB() == body1.mBody && joint->GetBodyA() == body2.mBody) {
-				joint->SetLength(length);
+				joint->SetLength(length * getCi2BoxScale());
 		}
 	}
 }
