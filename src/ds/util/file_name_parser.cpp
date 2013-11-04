6a119dc4e9c4d5c0d7bd71e7321d334442bf6dcf
eric hackborn <eric.hackborn@downstream.com>
2013-11-04 14:26:35 -0800

    Expand the file metadata parser to include arbitrary types.

--- src/ds/util/file_name_parser.cpp
+++ src/ds/util/file_name_parser.cpp
@@ -1,22 +1,78 @@
 #include "file_name_parser.h"
-#include "string_util.h"
+
 #include <map>
+#include <Poco/Path.h>
 
 namespace ds {
 
-ParseFileMetaException::ParseFileMetaException( const std::string &metaValue )
-  : mWhat(metaValue)
-{
-
+namespace {
+const std::string		EMPTY_SZ("");
 }
 
-const char *ParseFileMetaException::what() const throw()
-{
-  return mWhat.c_str();
+/**
+ * \class ds::ParseFileMetaException
+ */
+ParseFileMetaException::ParseFileMetaException(const std::string& metaValue)
+	: mWhat(metaValue) {
 }
 
-int parseFileMetaData( const std::string &filename, const std::string &metaValue )
-{
+const char *ParseFileMetaException::what() const throw() {
+	return mWhat.c_str();
+}
+
+/**
+ * \class ds::FileMetaData
+ */
+FileMetaData::FileMetaData() {
+}
+
+void FileMetaData::parse(const std::string& filename) {
+	mAttrib.clear();
+
+	Poco::Path			p(filename);
+	std::vector<std::string> splitString = ds::split(p.getBaseName(), ".");
+	for (auto it = splitString.begin(), it2 = splitString.end(); it != it2; ++it ) {
+		// First item is base name
+		if (it == splitString.begin()) continue;
+		std::string		str = *it;
+		const size_t	pos = str.find("_");
+		if (pos == std::string::npos) continue;
+
+		std::vector<std::string> meta = split(str, "_");
+		if (meta.size() != 2) continue;
+		mAttrib.push_back(std::pair<std::string, std::string>(meta.front(), meta.back()));
+	}
+}
+
+size_t FileMetaData::keyCount() const {
+	return mAttrib.size();
+}
+
+bool FileMetaData::contains(const std::string& key) const {
+	for (auto it=mAttrib.begin(), end=mAttrib.end(); it!=end; ++it) {
+		if (it->first == key) return true;
+	}
+	return false;
+}
+
+const std::string& FileMetaData::keyAt(const size_t index) const {
+	if (index >= mAttrib.size()) return EMPTY_SZ;
+	return mAttrib[index].first;
+}
+
+const std::string& FileMetaData::valueAt(const size_t index) const {
+	if (index >= mAttrib.size()) return EMPTY_SZ;
+	return mAttrib[index].second;
+}
+
+const std::string& FileMetaData::findValue(const std::string& key) const {
+	for (auto it=mAttrib.begin(), end=mAttrib.end(); it!=end; ++it) {
+		if (it->first == key) return it->second;
+	}
+	return EMPTY_SZ;
+}
+
+int parseFileMetaData( const std::string &filename, const std::string &metaValue ) {
   std::vector<std::string> splitString = ds::split(filename, ".");
 
   for ( auto it = splitString.begin(), it2 = splitString.end(); it != it2; ++it ) {
@@ -36,20 +92,23 @@ int parseFileMetaData( const std::string &filename, const std::string &metaValue
 
 static std::map<std::string, ci::Vec2f> ParsedAttribs;
 
-ci::Vec2f parseFileMetaDataSize(const std::string &filename)
-{
-  std::map<std::string, ci::Vec2f>::iterator found = ParsedAttribs.find(filename);
+ci::Vec2f parseFileMetaDataSize(const std::string &filename) {
+	std::map<std::string, ci::Vec2f>::iterator found = ParsedAttribs.find(filename);
 
-  if (found != ParsedAttribs.end())
-    return found->second;
+	if (found != ParsedAttribs.end())
+		return found->second;
 
-  int wVal = parseFileMetaData(filename, "w_");
-  int hVal = parseFileMetaData(filename, "h_");
+	int wVal = parseFileMetaData(filename, "w_");
+	int hVal = parseFileMetaData(filename, "h_");
 
-  ci::Vec2f values(static_cast<float>(wVal), static_cast<float>(hVal));
-  ParsedAttribs[filename] = values;
+	ci::Vec2f values(static_cast<float>(wVal), static_cast<float>(hVal));
+	ParsedAttribs[filename] = values;
 
-  return values;
+	return values;
+}
+
+void getFileMetaData(const std::string& filename, std::vector<std::pair<std::string, std::string>>& out) {
+	
 }
 
 }
--- src/ds/util/file_name_parser.h
+++ src/ds/util/file_name_parser.h
@@ -1,26 +1,64 @@
 #pragma once
 #ifndef DS_UTIL_FILE_NAME_PARSER_H
 #define DS_UTIL_FILE_NAME_PARSER_H
-#include <string>
+
 #include <exception>
-#include "cinder/Vector.h"
+#include <string>
+#include <vector>
+#include <cinder/Vector.h>
+#include <ds/util/string_util.h>
 
 
 namespace ds {
 
-class ParseFileMetaException : public std::exception
-{
-  public:
-    ParseFileMetaException(const std::string &metaValue);
-    const char *what() const throw();
-  private:
-    std::string mWhat;
+/**
+ * \class ds::ParseFileMetaException
+ */
+class ParseFileMetaException : public std::exception {
+public:
+	ParseFileMetaException(const std::string& metaValue);
+	const char *what() const throw();
+
+private:
+	std::string mWhat;
+};
+
+/**
+ * \class ds::FileMetaData
+ * Collection of file meta data.
+ */
+class FileMetaData {
+public:
+	FileMetaData();
+
+	void				parse(const std::string& filename);
+
+	size_t				keyCount() const;
+	bool				contains(const std::string& key) const;
+	const std::string&	keyAt(const size_t index) const;
+	const std::string&	valueAt(const size_t index) const;
+	const std::string&	findValue(const std::string& key) const;
+	template <typename T>
+	T					findValueType(const std::string& key, const T error) const;
+
+private:
+	std::vector<std::pair<std::string, std::string>>
+						mAttrib;
 };
 
 // throws ParseFileMetaException when parsing fails.
-int parseFileMetaData(const std::string &filename, const std::string &metaValue);
+int parseFileMetaData(const std::string& filename, const std::string& metaValue);
 // does not catch parseFileMetaData exception, so consider parseFileMetaDataSize throwing on failure.
-ci::Vec2f parseFileMetaDataSize(const std::string &filename);
+ci::Vec2f parseFileMetaDataSize(const std::string& filename);
+
+template <typename T>
+T FileMetaData::findValueType(const std::string& key, const T error) const {
+	const std::string&	v = findValue(key);
+	if (v.empty()) return error;
+	T					ans;
+	if (!ds::string_to_value(v, ans)) return error;
+	return ans;
+}
 
 }
 
