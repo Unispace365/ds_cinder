c81763fb1d09101d01e2307808f2bf6d55283a35
Eric Hackborn <hackborn@gmail.com>
2013-12-06 12:46:06 -0800

    Update PDF example with ability to step through pages.

--- example/pdf/src/starter_app.cpp
+++ example/pdf/src/starter_app.cpp
@@ -1,6 +1,5 @@
-#include <cinder/app/AppBasic.h>
-#include <ds/app/environment.h>
 #include <ds/app/app.h>
+#include <ds/app/environment.h>
 #include <ds/ui/sprite/pdf.h>
 
 using namespace std;
@@ -12,21 +11,50 @@ public:
 	BasicTweenApp();
 
 	void				setupServer();
+	void                keyDown(KeyEvent);
+	virtual void		mouseUp(MouseEvent);
 
 private:
 	typedef ds::App		inherited;
+	ds::ui::Pdf*		mPdf;
 };
 
-BasicTweenApp::BasicTweenApp() {
+BasicTweenApp::BasicTweenApp()
+		: mPdf(nullptr) {
 }
 
 void BasicTweenApp::setupServer() {
+	std::cout << "Press \"+\" and \"-\" to navigate pages." << std::endl;
+	std::cout << "Hmm for some reason it's not getting key presses. Use mouse left/right instead." << std::endl;
+
 	ds::ui::Sprite &rootSprite = mEngine.getRootSprite();
 
 	ds::ui::Pdf&	pdf = ds::ui::Sprite::makeAlloc<ds::ui::Pdf>([this]()->ds::ui::Pdf*{return new ds::ui::Pdf(this->mEngine);}, &rootSprite);
+	mPdf = &pdf;
 	pdf.setResourceFilename(ds::Environment::getAppFolder("data", "bitcoin.pdf"));
 	pdf.setCenter(0.5f, 0.5f);
 	pdf.setPosition(floorf(mEngine.getWorldWidth()/2.0f), floorf(mEngine.getWorldHeight()/2.0f));
+}
+
+void BasicTweenApp::keyDown(KeyEvent e) {
+	inherited::keyDown(e);
+
+	if (e.getCode() == KeyEvent::KEY_ESCAPE) {
+		quit();
+	} else if (e.getCode() == KeyEvent::KEY_PLUS) {
+		if (mPdf) mPdf->goToNextPage();
+	} else if (e.getCode() == KeyEvent::KEY_MINUS) {
+		if (mPdf) mPdf->goToPreviousPage();
+	}
+}
+
+void BasicTweenApp::mouseUp(MouseEvent e) {
+	inherited::mouseUp(e);
+
+	if (mPdf) {
+		if (e.isLeft()) mPdf->goToPreviousPage();
+		else if (e.isRight()) mPdf->goToNextPage();
+	}
 }
 
 // This line tells Cinder to actually create the application
