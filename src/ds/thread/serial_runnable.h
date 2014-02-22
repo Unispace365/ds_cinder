9a6b39dda3bc753d0b952c0655df2d0fc2f2c254
eric hackborn <eric.hackborn@downstream.com>
2014-02-21 16:18:10 -0800

    Make a tweak to SerialRunnable that means clients are no longer required to have an empty constructor -- just supply the desired allocator.

--- src/ds/thread/serial_runnable.h
+++ src/ds/thread/serial_runnable.h
@@ -36,113 +36,111 @@ public:
 	typedef std::function<void (T&)>	HandlerFunc;
 
 public:
-	// If T has a constructor without arguments, ignore the alloc. If you need
-	// to supply info to the constructor, supply a custom alloc.
-	SerialRunnable(ui::SpriteEngine&, const std::function<T*(void)>& alloc = nullptr);
+	SerialRunnable(	ui::SpriteEngine&,
+					// If T has a constructor without arguments, ignore the alloc. If you need
+					// to supply info to the constructor, supply a custom alloc.
+					const std::function<T*(void)>& alloc = []()->T*{return new T();});
 	
-	void							  setReplyHandler(const HandlerFunc& f) { mReplyHandler = f; }
+	void					setReplyHandler(const HandlerFunc& f) { mReplyHandler = f; }
 	// Start a new runnable, intializing it via the handler block.  Any previous, unfinished runs
 	// will be ignored.
-  // If waitForResult is true, this will be run synchronously, blocking until the operation is finished.
-  // This is useful for serial runnables that run through the life of an app, but during setup you
-  // want to run them once and guarantee data exists before the app begins.
-  // NOTE: Be careful with HandlerFunc; it might be evaluated at some later point. So if you
-  // pass in a lambda with captured variables, make sure to capture by value, not reference, or if
-  // you do, that the referenced item still exists for the life of the SerialRunnable.
-	bool							  start(const HandlerFunc& = nullptr, const bool waitForResult = false);
+	// If waitForResult is true, this will be run synchronously, blocking until the operation is finished.
+	// This is useful for serial runnables that run through the life of an app, but during setup you
+	// want to run them once and guarantee data exists before the app begins.
+	// NOTE: Be careful with HandlerFunc; it might be evaluated at some later point. So if you
+	// pass in a lambda with captured variables, make sure to capture by value, not reference, or if
+	// you do, that the referenced item still exists for the life of the SerialRunnable.
+	bool					start(const HandlerFunc& = nullptr, const bool waitForResult = false);
 
 private:
-	bool							  send();
-	void								receive(std::unique_ptr<Poco::Runnable>&);
+	bool					send();
+	void					receive(std::unique_ptr<Poco::Runnable>&);
 
-  RunnableClient      mClient;
-  enum State {
-      CACHED,         // mCache is valid, nothing is running --NOTE this state is redundant with mCache existing, so favour that
-      RUNNING,        // mCache is empty, and as soon as I receive something, I'm done
-      WAITING         // mCache is empty, as soon as I receive something, I start again
-  };
-  State               mState;
-  std::unique_ptr<T>  mCache;
-	HandlerFunc				  mReplyHandler;
-  // If start comes in and I can't start yet, cache the start handler
-  HandlerFunc         mStartHandler;
+	RunnableClient			mClient;
+	enum State {
+		CACHED,				// mCache is valid, nothing is running --NOTE this state is redundant with mCache existing, so favour that
+		RUNNING,			// mCache is empty, and as soon as I receive something, I'm done
+		WAITING				// mCache is empty, as soon as I receive something, I start again
+	};
+	State					mState;
+	std::unique_ptr<T>		mCache;
+	HandlerFunc				mReplyHandler;
+	// If start comes in and I can't start yet, cache the start handler
+	HandlerFunc				mStartHandler;
 };
 
 template <class T>
 SerialRunnable<T>::SerialRunnable(ui::SpriteEngine& se, const std::function<T*(void)>& alloc)
-	: mClient(se)
-  , mState(CACHED)
-	, mReplyHandler(nullptr)
-  , mStartHandler(nullptr)
-{
-  // Create the single runnable
-  std::unique_ptr<T>    up(alloc == nullptr ? new T : alloc());
-  mCache = std::move(up);
-  if (!mCache) throw std::exception("Can't allocate serial runnable");
+		: mClient(se)
+		, mState(CACHED)
+		, mReplyHandler(nullptr)
+		, mStartHandler(nullptr) {
+	// Create the single runnable
+	if (!alloc) throw std::exception("Can't allocate serial runnable (no allocator)");
+	std::unique_ptr<T>		up(alloc());
+	mCache = std::move(up);
+	if (!mCache) throw std::exception("Can't allocate serial runnable");
 
-  mClient.setResultHandler([this](std::unique_ptr<Poco::Runnable>& r) { receive(r); });
+	mClient.setResultHandler([this](std::unique_ptr<Poco::Runnable>& r) { receive(r); });
 }
 
 template <class T>
-bool SerialRunnable<T>::start(const HandlerFunc& f, const bool waitForResult)
-{
-  // Waiting for a result with an operation currently running is an error.
-  if (waitForResult) {
-    if (!mCache) return false;
+bool SerialRunnable<T>::start(const HandlerFunc& f, const bool waitForResult) {
+	// Waiting for a result with an operation currently running is an error.
+	if (waitForResult) {
+		if (!mCache) return false;
 
-    if (f) f(*(mCache.get()));
-    mCache->run();
-    std::unique_ptr<Poco::Runnable>		payload(ds::unique_dynamic_cast<Poco::Runnable, T>(mCache));
-    mState = RUNNING; // make sure the result handler is triggered
-    receive(payload);
-    return true;
-  }
+		if (f) f(*(mCache.get()));
+		mCache->run();
+		std::unique_ptr<Poco::Runnable>		payload(ds::unique_dynamic_cast<Poco::Runnable, T>(mCache));
+		mState = RUNNING; // make sure the result handler is triggered
+		receive(payload);
+		return true;
+	}
 
-  mStartHandler = f;
+	mStartHandler = f;
 
-  // If not waiting, then either start or indicate we need to start.
-  if (mCache) {
-    mState = CACHED;
-    return send();
-  } else {
-    mState = WAITING;
-  }
-  return true;
+	// If not waiting, then either start or indicate we need to start.
+	if (mCache) {
+		mState = CACHED;
+		return send();
+	} else {
+		mState = WAITING;
+	}
+	return true;
 }
 
 template <class T>
-bool SerialRunnable<T>::send()
-{
-  if (!mCache) return false;
-  if (mStartHandler) {
-    mStartHandler(*(mCache.get()));
-    mStartHandler = nullptr;
-  }
-  std::unique_ptr<Poco::Runnable>		payload(ds::unique_dynamic_cast<Poco::Runnable, T>(mCache));
-  if (!payload) return false;
+bool SerialRunnable<T>::send() {
+	if (!mCache) return false;
+	if (mStartHandler) {
+		mStartHandler(*(mCache.get()));
+		mStartHandler = nullptr;
+	}
+	std::unique_ptr<Poco::Runnable>		payload(ds::unique_dynamic_cast<Poco::Runnable, T>(mCache));
+	if (!payload) return false;
 
-  if (!mClient.run(payload)) return false;
+	if (!mClient.run(payload)) return false;
 
-  mState = RUNNING;
-  return true;
+	mState = RUNNING;
+	return true;
 }
 
 template <class T>
-void SerialRunnable<T>::receive(std::unique_ptr<Poco::Runnable>& r)
-{
+void SerialRunnable<T>::receive(std::unique_ptr<Poco::Runnable>& r) {
 	std::unique_ptr<T>		payload(ds::unique_dynamic_cast<T, Poco::Runnable>(r));
 	if (payload) {
-    if (mState != WAITING) {
-		  if (mReplyHandler) mReplyHandler(*(payload.get()));
-    }
-    mCache = std::move(payload);
-  }
+		if (mState != WAITING) {
+			if (mReplyHandler) mReplyHandler(*(payload.get()));
+		}
+		mCache = std::move(payload);
+	}
 
-  if (mState == WAITING) {
-    send();
-  } else {
-    mState = CACHED;
-  }
+	if (mState == WAITING) {
+		send();
+	} else {
+		mState = CACHED;
+	}
 }
 
 } // namespace ds
