690581998648b60d2f0fb7a1b2f6acf6630e53e6
eric hackborn <eric.hackborn@downstream.com>
2014-10-20 09:59:23 -0700

    Reenable my global text rotation, confirmed working

--- src/ds/ui/touch/rotation_translator.cpp
+++ src/ds/ui/touch/rotation_translator.cpp
@@ -15,51 +15,37 @@ RotationTranslator::RotationTranslator() {
 
 void RotationTranslator::down(TouchInfo &ti) {
 	if (!(ti.mPickedSprite && ti.mPickedSprite->isRotateTouches())) return;
-#if 0
+
 	mMatrix[ti.mFingerId] = buildRotationMatrix(ti.mPickedSprite);
-std::cout << "down assign for " << ti.mFingerId << std::endl;
-#endif
 }
 
 void RotationTranslator::move(TouchInfo &ti, const ci::Vec3f &previous_global_pt) {
 	if (!(ti.mPickedSprite && ti.mPickedSprite->isRotateTouches())) return;
 
-#if 0
 	ci::Matrix44f			m(ci::Matrix44f::identity());
 
 	auto f = mMatrix.find(ti.mFingerId);
 	if (f != mMatrix.end()) {
 		m = f->second;
-std::cout << "move for " << ti.mFingerId << std::endl;
 	} else {
 		m = buildRotationMatrix(ti.mPickedSprite);
 		mMatrix[ti.mFingerId] = m;
-std::cout << "move assign for " << ti.mFingerId << std::endl;
 	}
-
-m = buildRotationMatrix(ti.mPickedSprite);
-
-m = ci::Matrix44f::identity();
-m.rotate(ci::Vec3f(1.0f, 0.0f, 0.0f), 0.0f * math::DEGREE2RADIAN);
-m.rotate(ci::Vec3f(0.0f, 1.0f, 0.0f), 0.0f * math::DEGREE2RADIAN);
-m.rotate(ci::Vec3f(0.0f, 0.0f, 1.0f), 180.0f * math::DEGREE2RADIAN);
 
 	ci::Vec3f				pt(ti.mCurrentGlobalPoint - ti.mStartPoint);
 	pt = ti.mStartPoint + m.transformPoint(pt);
 	ti.mCurrentGlobalPoint = pt;
 	ti.mDeltaPoint = ti.mCurrentGlobalPoint - previous_global_pt;
-#endif
 }
 
 void RotationTranslator::up(TouchInfo &ti) {
 	if (mMatrix.empty()) return;
-#if 0
+
 	try {
 		auto f = mMatrix.find(ti.mFingerId);
 		if (f != mMatrix.end()) mMatrix.erase(f);
 	} catch (std::exception const&) {
 	}
-#endif
 }
 
 ci::Matrix44f RotationTranslator::buildRotationMatrix(ds::ui::Sprite *s) const {
@@ -72,10 +58,8 @@ ci::Matrix44f RotationTranslator::buildRotationMatrix(ds::ui::Sprite *s) const {
 		vec.push_back(s);
 		s = s->getParent();
 	}
-std::cout << "build rotation" << std::endl;
 	for (auto it=vec.rbegin(), end=vec.rend(); it!=end; ++it) {
 		const ci::Vec3f				rotation((*it)->getRotation());
-std::cout << "\t" << rotation << std::endl;
 		m.rotate(ci::Vec3f(1.0f, 0.0f, 0.0f), rotation.x * math::DEGREE2RADIAN);
 		m.rotate(ci::Vec3f(0.0f, 1.0f, 0.0f), rotation.y * math::DEGREE2RADIAN);
 		m.rotate(ci::Vec3f(0.0f, 0.0f, 1.0f), rotation.z * math::DEGREE2RADIAN);
