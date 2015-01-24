5c40be797d4e7962ce79d87782890bedc21b0541
Sepehr Laal <sepehr.laal@gmail.com>
2015-01-23 16:47:45 -0800

    Re-implemented Uniform class to make it network-transport ready

--- src/ds/gl/uniform.cpp
+++ src/ds/gl/uniform.cpp
@@ -1,119 +1,310 @@
 #include "ds/gl/uniform.h"
+#include <cinder/gl/GlslProg.h>
 
 namespace ds {
 namespace gl {
 
 namespace {
+static std::string EMPTY_SZ("");
+static UniformData EMPTY_DATA;
+} // !anonymous namespace
 
-template<typename T>
-void apply(const std::map<std::string, T>& data, ci::gl::GlslProg& shader) {
-	if (!data.empty()) {
-		for (auto it=data.begin(), end=data.end(); it!=end; ++it) {
-			shader.uniform(it->first, it->second);
-		}
-	}
+UniformData::UniformData(int count /*= 0*/, bool transpose /*= false*/)
+	: mCount(count)
+	, mTranspose(transpose)
+{}
+
+bool UniformData::operator==(const UniformData& rhs) const
+{
+	return rhs.mTranspose == mTranspose && rhs.mCount == mCount;
 }
 
-template<typename T>
-void apply_vec(const std::map<std::string, T>& data, ci::gl::GlslProg& shader) {
-	if (!data.empty()) {
-		for (auto it=data.begin(), end=data.end(); it!=end; ++it) {
-			if (it->second.size() > 0) {
-				shader.uniform(it->first, &(it->second.front()), it->second.size());
-			}
-		}
-	}
+UniformVisitor::UniformVisitor(ci::gl::GlslProg& shader)
+	: mShader(shader)
+	, mData(EMPTY_DATA)
+	, mName(EMPTY_SZ)
+{}
+
+void UniformVisitor::operator()(int data)
+{
+	mShader.uniform(mName, data);
 }
 
+void UniformVisitor::operator()(const ci::Vec2i &data)
+{
+	mShader.uniform(mName, data);
+}
+
+void UniformVisitor::operator()(const int *data)
+{
+	mShader.uniform(mName, data, mData.mCount);
+}
+
+void UniformVisitor::operator()(const ci::Vec2i *data)
+{
+	mShader.uniform(mName, data, mData.mCount);
+}
+
+void UniformVisitor::operator()(float data)
+{
+	mShader.uniform(mName, data);
+}
+
+void UniformVisitor::operator()(const ci::Vec2f &data)
+{
+	mShader.uniform(mName, data);
+}
+
+void UniformVisitor::operator()(const ci::Vec3f &data)
+{
+	mShader.uniform(mName, data);
+}
+
+void UniformVisitor::operator()(const ci::Vec4f &data)
+{
+	mShader.uniform(mName, data);
+}
+
+void UniformVisitor::operator()(const ci::Color &data)
+{
+	mShader.uniform(mName, data);
+}
+
+void UniformVisitor::operator()(const ci::ColorA &data)
+{
+	mShader.uniform(mName, data);
+}
+
+void UniformVisitor::operator()(const ci::Matrix22f &data)
+{
+	mShader.uniform(mName, data, mData.mTranspose);
+}
+
+void UniformVisitor::operator()(const ci::Matrix33f &data)
+{
+	mShader.uniform(mName, data, mData.mTranspose);
+}
+
+void UniformVisitor::operator()(const ci::Matrix44f &data)
+{
+	mShader.uniform(mName, data, mData.mTranspose);
+}
+
+void UniformVisitor::operator()(const float *data)
+{
+	mShader.uniform(mName, data, mData.mCount);
+}
+
+void UniformVisitor::operator()(const ci::Vec2f *data)
+{
+	mShader.uniform(mName, data, mData.mCount);
+}
+
+void UniformVisitor::operator()(const ci::Vec3f *data)
+{
+	mShader.uniform(mName, data, mData.mCount);
+}
+
+void UniformVisitor::operator()(const ci::Vec4f *data)
+{
+	mShader.uniform(mName, data, mData.mCount);
+}
+
+void UniformVisitor::operator()(const ci::Matrix22f *data)
+{
+	mShader.uniform(mName, data, mData.mCount, mData.mTranspose);
+}
+
+void UniformVisitor::operator()(const ci::Matrix33f *data)
+{
+	mShader.uniform(mName, data, mData.mCount, mData.mTranspose);
+}
+
+void UniformVisitor::operator()(const ci::Matrix44f *data)
+{
+	mShader.uniform(mName, data, mData.mCount, mData.mTranspose);
 }
 
 /**
  * \class ds::gl::Uniform
  */
 Uniform::Uniform()
-		: mIsEmpty(true)
-		, mEmptyDirty(true) {
-}
+	: mIsEmpty(true)
+	, mEmptyDirty(true)
+{}
 
 bool Uniform::operator==(const Uniform& o) const {
-	if (mFloat != o.mFloat) return false;
-	if (mFloats != o.mFloats) return false;
-	if (mInt != o.mInt) return false;
-	if (mMatrix44f != o.mMatrix44f) return false;
-	if (mVec2i != o.mVec2i) return false;
-	if (mVec4f != o.mVec4f) return false;
+	if (mVariantUniforms != o.mVariantUniforms) return false;
 	return true;
 }
 
 bool Uniform::empty() const {
 	if (mEmptyDirty) {
 		mIsEmpty = true;
-		if (!mFloat.empty()) mIsEmpty = false;
-		else if (!mFloats.empty()) mIsEmpty = false;
-		else if (!mInt.empty()) mIsEmpty = false;
-		else if (!mMatrix44f.empty()) mIsEmpty = false;
-		else if (!mVec2i.empty()) mIsEmpty = false;
-		else if (!mVec4f.empty()) mIsEmpty = false;
+		if (!mVariantUniforms.empty()) return false;
 		mEmptyDirty = false;
 	}
 	return mIsEmpty;
 }
 
 void Uniform::clear() {
-	mFloat.clear();
-	mFloats.clear();
-	mInt.clear();
-	mMatrix44f.clear();
-	mVec2i.clear();
-	mVec4f.clear();
+	mVariantUniforms.clear();
 	mIsEmpty = true;
 	mEmptyDirty = false;
 }
 
 void Uniform::setFloat(const std::string& key, const float value) {
 	if (key.empty()) return;
-	mFloat[key] = value;
-	mEmptyDirty = true;
+	set(key, value);
 }
 
 void Uniform::setFloats(const std::string& key, const std::vector<float>& value) {
 	if (key.empty()) return;
-	mFloats[key] = value;
-	mEmptyDirty = true;
-	mEmptyDirty = true;
+	setInternal(key, UniformVisitor::SupportedVariants((float*)value.data()), UniformData(value.size()));
 }
 
 void Uniform::setInt(const std::string& key, const int value) {
 	if (key.empty()) return;
-	mInt[key] = value;
-	mEmptyDirty = true;
+	set(key, value);
 }
 
 void Uniform::setMatrix44f(const std::string& key, const ci::Matrix44f& value) {
 	if (key.empty()) return;
-	mMatrix44f[key] = value;
-	mEmptyDirty = true;
+	set(key, value);
 }
 
 void Uniform::setVec2i(const std::string& key, const ci::Vec2i& value) {
 	if (key.empty()) return;
-	mVec2i[key] = value;
-	mEmptyDirty = true;
+	set(key, value);
 }
 
 void Uniform::setVec4f(const std::string& key, const ci::Vec4f& value) {
 	if (key.empty()) return;
-	mVec4f[key] = value;
-	mEmptyDirty = true;
+	set(key, value);
 }
 
 void Uniform::applyTo(ci::gl::GlslProg& shader) const {
-	apply<float>(mFloat, shader);
-	apply_vec<std::vector<float>>(mFloats, shader);
-	apply<int>(mInt, shader);
-	apply<ci::Matrix44f>(mMatrix44f, shader);
-	apply<ci::Vec2i>(mVec2i, shader);
-	apply<ci::Vec4f>(mVec4f, shader);
+	if (!mVariantUniforms.empty())
+	{
+		// constructing this is really cheap. so no worries.
+		auto local_visitor = UniformVisitor(shader);
+
+		for (auto it = mVariantUniforms.cbegin(), end = mVariantUniforms.cend(); it != end; ++it)
+		{
+			local_visitor.mData = it->second.second;
+			local_visitor.mName = it->first;
+			boost::apply_visitor(local_visitor, it->second.first);
+		}
+	}
+}
+
+void Uniform::setInternal(const std::string& key, const UniformVisitor::SupportedVariants& val, const UniformData& data)
+{
+	if (key.empty()) return;
+	mVariantUniforms[key] = std::make_pair(val, data);
+	mEmptyDirty = true;
+}
+
+// set overloads
+
+void Uniform::set(const std::string &name, int data)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData());
+}
+
+void Uniform::set(const std::string &name, const ci::Vec2i &data)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData());
+}
+
+void Uniform::set(const std::string &name, const int *data, int count)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(count));
+}
+
+void Uniform::set(const std::string &name, const ci::Vec2i *data, int count)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(count));
+}
+
+void Uniform::set(const std::string &name, float data)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData());
+}
+
+void Uniform::set(const std::string &name, const ci::Vec2f &data)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData());
+}
+
+void Uniform::set(const std::string &name, const ci::Vec3f &data)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData());
+}
+
+void Uniform::set(const std::string &name, const ci::Vec4f &data)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData());
+}
+
+void Uniform::set(const std::string &name, const ci::Color &data)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData());
+}
+
+void Uniform::set(const std::string &name, const ci::ColorA &data)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData());
+}
+
+void Uniform::set(const std::string &name, const ci::Matrix22f &data, bool transpose /*= false*/)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(0, transpose));
+}
+
+void Uniform::set(const std::string &name, const ci::Matrix33f &data, bool transpose /*= false*/)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(0, transpose));
+}
+
+void Uniform::set(const std::string &name, const ci::Matrix44f &data, bool transpose /*= false*/)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(0, transpose));
+}
+
+void Uniform::set(const std::string &name, const float *data, int count)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(count));
+}
+
+void Uniform::set(const std::string &name, const ci::Vec2f *data, int count)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(count));
+}
+
+void Uniform::set(const std::string &name, const ci::Vec3f *data, int count)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(count));
+}
+
+void Uniform::set(const std::string &name, const ci::Vec4f *data, int count)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(count));
+}
+
+void Uniform::set(const std::string &name, const ci::Matrix22f *data, int count, bool transpose /*= false*/)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(count, transpose));
+}
+
+void Uniform::set(const std::string &name, const ci::Matrix33f *data, int count, bool transpose /*= false*/)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(count, transpose));
+}
+
+void Uniform::set(const std::string &name, const ci::Matrix44f *data, int count, bool transpose /*= false*/)
+{
+	setInternal(name, UniformVisitor::SupportedVariants(data), UniformData(count, transpose));
 }
 
 } // namespace gl
