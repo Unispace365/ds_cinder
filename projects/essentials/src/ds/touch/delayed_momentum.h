faa9852f9ae58f5125951dcc76f4def489af5221
Gordon Nickel <gordon.nickel@downstream.com>
2015-02-09 17:15:50 -0800

    Add delayed_momentum to essentials project.

---
+++ projects/essentials/src/ds/touch/delayed_momentum.h
@@ -0,0 +1,60 @@
+#pragma once
+#ifndef EXXON_TOUCH_DELAYED_MOMENTUM_H_
+#define EXXON_TOUCH_DELAYED_MOMENTUM_H_
+
+#include <vector>
+#include <cinder/Vector.h>
+#include <Poco/Timestamp.h>
+#include <ds/app/auto_update.h>
+#include <ds/ui/sprite/sprite_engine.h>
+
+namespace ds {
+namespace ui {
+
+/**
+* \class ds::ui::DelayedMomentum
+* A helper class to have sprites move with momentum in 2d.
+* This has more of a 'pull' feel than the regular momentum. This is intended for during the drag phase, and not so much after it.
+* Currently single-direction only.
+*/
+
+class DelayedMomentum final : public ds::AutoUpdate {
+public:
+	DelayedMomentum(ds::ui::SpriteEngine& enging, const float deltaBurndown = 0.95f, const int maxSmoothFrames = 20);
+
+	// Removes all delta touches and current delta is set to 0.0
+	void						clear();
+
+	// add a movement touch point
+	void						addDeltaPosition(const float delta);
+
+	// get the most recent value
+	const float					getDelta();
+
+	// Delta burn down is how quickly to fade off old inputs. Kind of like friction
+	void						setDeltaBurndown(const float burndown){ mDeltaBurndown = burndown; }
+	const float					getDeltaBurndown(){ return mDeltaBurndown; }
+
+	// Smooth frames is the absolute number of inputs to keep.
+	// Inputs after this amount get discarded
+	void						setNumSmoothFrames(const int smoothFrams){ mNumSmoothFrames = smoothFrams; }
+	const int					getNumSmoothFrames(){ return mNumSmoothFrames; }
+
+protected:
+	virtual void				update(const ds::UpdateParams&);
+
+private:
+	typedef ds::AutoUpdate		inherited;
+
+
+	std::vector<float>			mDeltas;
+	float						mCurrentDelta;
+	float						mDeltaBurndown;
+	int							mNumSmoothFrames;
+
+};
+
+} // namespace ui
+} // namespace ds
+
+#endif // EXXON_TOUCH_DELAYED_MOMENTUM_H_
