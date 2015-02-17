bb52b8a170b1a8f751a4a55eb756a3fafb98a943
Gordon Nickel <gordon.nickel@downstream.com>
2015-02-17 13:34:35 -0800

    Add circle crop example project.

---
+++ example/circle_crop_example/src/query/story_query.h
@@ -0,0 +1,29 @@
+#ifndef _CIRCLECROPEXAMPLE_APP_QUERY_INDUSTRY_QUERY_H_
+#define _CIRCLECROPEXAMPLE_APP_QUERY_INDUSTRY_QUERY_H_
+
+#include <functional>
+#include <Poco/Runnable.h>
+#include <ds/query/query_result.h>
+
+#include "model/all_stories.h"
+
+namespace example {
+
+/**
+ * \class example::StoryQuery
+ */
+class StoryQuery : public Poco::Runnable {
+public:
+	StoryQuery();
+
+	virtual void			run();
+
+	AllStories				mOutput;
+
+private:
+	void					query(AllStories& output);
+};
+
+} // !namespace example
+
+#endif //!_CIRCLECROPEXAMPLE_APP_QUERY_INDUSTRY_QUERY_H_
