a082065503c5aa191f3c981e24dc50007e8989c6
Sepehr Laal <sepehr.laal@gmail.com>
2015-07-16 08:30:37 -0700

    GST sprite: Re-ordered the construction order. Normalized whitespace.
    Wrapper pointer needs mNetHandler to be constructed first. Also arranged
    members so they respect memory alignment of the class.

--- projects/video/gstreamer-1.0/src/ds/ui/sprite/gst_video.h
+++ projects/video/gstreamer-1.0/src/ds/ui/sprite/gst_video.h
@@ -140,44 +140,39 @@ private:
 	void				setStatus(const int);
 	void				checkStatus();
 
-protected:
-	gstwrapper::GStreamerWrapper*		mGstreamerWrapper;
-
 private:
-
-	GstVideoNet							mNetHandler;
-
-	ci::gl::Texture						mFrameTexture;
-	ci::Vec2i							mVideoSize;
-	std::string							mFilename;
-
-	bool								mLooping;
-
-	bool								mMuted;
-	bool								mOutOfBoundsMuted;
-	float								mVolume;
-
-	bool								mAutoStart;
-	bool								mShouldPlay;
-	bool								mShouldSync;
-
-	// There is actual video data ready to be drawn
-	bool								mDrawable;
-
-	// When the video is playing, extend the idle timer, or not
-	bool								mAutoExtendIdle;
+	GstVideoNet			mNetHandler;
 	
+protected:
+	gstwrapper::GStreamerWrapper*
+						mGstreamerWrapper;
+private:
+	ci::gl::Texture		mFrameTexture;
+	ci::Vec2i			mVideoSize;
+	std::string			mFilename;
+	Status				mStatus;
+	std::function<void()>
+						mPlaySingleFrameFunction;
+	std::function<void()>
+						mVideoCompleteFn;
+	std::function<void(const Status&)>
+						mStatusFn;
+	float				mVolume;
+	bool				mLooping;
+	bool				mMuted;
+	bool				mOutOfBoundsMuted;
+	bool				mAutoStart;
+	bool				mShouldPlay;
+	bool				mShouldSync;
+	// There is actual video data ready to be drawn
+	bool				mDrawable;
+	// When the video is playing, extend the idle timer, or not
+	bool				mAutoExtendIdle;
 	// Playing a single frame, then stopping.
-	bool								mPlaySingleFrame;
-	std::function<void()>				mPlaySingleFrameFunction;
-
-	Status								mStatus;
-	bool								mStatusChanged;
-	std::function<void()>				mVideoCompleteFn;
-	std::function<void(const Status&)>	mStatusFn;
-
+	bool				mPlaySingleFrame;
+	bool				mStatusChanged;
 	//Allow for custom audio output
-	bool								mGenerateAudioBuffer;
+	bool				mGenerateAudioBuffer;
 };
 
 } //!namespace ui
