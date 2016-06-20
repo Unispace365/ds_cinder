ae1b4dd021a3bf5b5460f6787867b2d5174ef36c
gilgamesh212 <gilgamesh212@gmail.com>
2016-06-20 16:54:02 -0700

    add a parameter to set scroll direction for scroll area

--- projects/essentials/src/ds/ui/interface_xml/interface_xml_importer.cpp
+++ projects/essentials/src/ds/ui/interface_xml/interface_xml_importer.cpp
@@ -761,7 +761,7 @@ void XmlImporter::setSpriteProperty(ds::ui::Sprite &sprite, const std::string& p
 			DS_LOG_WARNING("Trying to set incompatible attribute _" << property << "_ on sprite of type: " << typeid(sprite).name());
 		}
 	}
-
+	// Scroll sprite properties
 	else if(property == "scroll_list_layout"){
 		auto scrollList = dynamic_cast<ds::ui::ScrollList*>(&sprite);
 		if(scrollList){
@@ -770,9 +770,7 @@ void XmlImporter::setSpriteProperty(ds::ui::Sprite &sprite, const std::string& p
 		} else {
 			DS_LOG_WARNING("Trying to set incompatible attribute _" << property << "_ on sprite of type: " << typeid(sprite).name());
 		}
-	}
-
-	else if(property == "scroll_list_animate"){
+	}else if(property == "scroll_list_animate"){
 		auto scrollList = dynamic_cast<ds::ui::ScrollList*>(&sprite);
 		if(scrollList){
 			auto vec = parseVector(value);
@@ -780,9 +778,7 @@ void XmlImporter::setSpriteProperty(ds::ui::Sprite &sprite, const std::string& p
 		} else {
 			DS_LOG_WARNING("Trying to set incompatible attribute _" << property << "_ on sprite of type: " << typeid(sprite).name());
 		}
-	}
-
-	else if(property == "scroll_fade_colors"){
+	}else if(property == "scroll_fade_colors"){
 		auto scrollList = dynamic_cast<ds::ui::ScrollList*>(&sprite);
 		ds::ui::ScrollArea* scrollArea = nullptr;
 		if(scrollList){
@@ -804,6 +800,15 @@ void XmlImporter::setSpriteProperty(ds::ui::Sprite &sprite, const std::string& p
 			}
 		} else {
 			DS_LOG_WARNING("Couldn't set scroll_fade_colors for this sprite ");
+		}
+	}else if (property == "scroll_area_vert"){
+		auto scrollArea = dynamic_cast<ds::ui::ScrollArea*>(&sprite);
+		if (scrollArea){
+			auto vec = parseBoolean(value);
+			scrollArea->setVertical(vec);
+		}
+		else {
+			DS_LOG_WARNING("Trying to set incompatible attribute _" << property << "_ on sprite of type: " << typeid(sprite).name());
 		}
 	}
 
