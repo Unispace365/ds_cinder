c77051f09998025879d680380ee4eafaa171def3
gilgamesh212 <gilgamesh212@gmail.com>
2016-09-08 08:35:45 -0700

    add SMTP Request to essentials/ds/net/network

---
+++ projects/essentials/src/ds/network/smtp_request.h
@@ -0,0 +1,63 @@
+#pragma once
+#ifndef ESSENTIALS_DS_NETWORLD_SMTP_REQUEST
+#define ESSENTIALS_DS_NETWORLD_SMTP_REQUEST
+
+#include <string>
+#include <functional>
+#include <vector>
+#include <ds/ui/sprite/sprite_engine.h>
+#include <ds/thread/parallel_runnable.h>
+#include <Poco/Runnable.h>
+
+namespace ds {
+namespace net {
+/**
+* \class ds::net::SMTPRequest
+* Make very simple SMTP requests
+*/
+
+class SMTPRequest {
+public:
+	SMTPRequest(ds::ui::SpriteEngine& eng);
+
+
+	/// The serverUrl is the SMTP SERVER url
+	// userInfo, first is email address for sender, second is email address for receiver, and following is email address for cc. format is "<example@email.com>"
+	// sInput is the content for the email, include from,to,cc,subject,content body. details see SMTP sample
+	// userpwd is the username and password for SMTP account. format is "user:pwd"
+
+
+	void					makePostRequest(const std::string serverUrl, const std::vector<std::string> userInfo, std::vector<std::string> sInput, std::string userpwd = "");
+
+	/// If errored == true, then something went wrong and the reply will have the error message
+	/// Otherwise it will be whatever was returned from the server
+	void					setReplyFunction(std::function<void(const bool errored, const std::string& reply)> func){ mReplyFunction = func; }
+
+private:
+	class IndividualRequest : public Poco::Runnable {
+	public:
+		IndividualRequest();
+
+		virtual void		run();
+
+		bool				mError;
+		std::string			mErrorMessage;
+
+		std::string			mOutput;
+
+		std::string			mFrom;
+		std::string			mTo;
+		std::vector<std::string>
+							mCC;
+		std::string			mUserPwd;
+		std::string			mSMTPServer;
+	};
+
+	void									onRequestComplete(IndividualRequest&);
+	ds::ParallelRunnable<IndividualRequest>	mRequests;
+	std::function<void(const bool errored, const std::string&)>	mReplyFunction;
+};
+} // namespace net
+} // namespace ds
+
+#endif 
