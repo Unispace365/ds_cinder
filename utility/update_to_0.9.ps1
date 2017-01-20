0683a3e6a10482ed21404adc13d1acba7bee0693
Gordon Nickel <gordon.nickel@downstream.com>
2017-01-19 18:09:18 -0800

    Add more find/replaces to the update powershell script

--- utility/update_to_0.9.ps1
+++ utility/update_to_0.9.ps1
@@ -1,3 +1,5 @@
+Write-Debug "Replacing vectors and app launch macros...";
+
 # Get a list of the source files
 $FileList = Get-ChildItem -Path .\ -Include *.cpp,*.h -Recurse;
 
@@ -10,11 +12,34 @@ foreach ($File in $FileList) {
 		Replace("ci::Vec2i", "ci::ivec2").
 		Replace("ci::Vec3f", "ci::vec3").
 		Replace("ci::Vec3i", "ci::ivec3").
+		Replace("ci::Vec4f", "ci::vec4").
+		Replace("ci::Vec4i", "ci::ivec4").
+		Replace("ci::Matrix44f", "ci::mat4").
+		Replace("ci::Matrix33f", "ci::mat3").
 		Replace("<cinder/app/AppBasic.h>", "<cinder/app/App.h>").
 		Replace("CINDER_APP_BASIC", "CINDER_APP");
-		Replace("RendererGl(RendererGl::Options", "ci::app::RendererGl(ci::app::RendererGl::Options");
+//		Replace('RendererGl(RendererGl::Options', 'ci::app::RendererGl(ci::app::RendererGl::Options');
 	
     Set-Content -Path $OutputFile -Value $Content;
 }
+
+Write-Host "Updating solution and project files...";
+
+# Get a list of the source files
+$FileListSolution = Get-ChildItem -Path .\ -Include *.sln,*.vcxproj -Recurse;
+
+# For each file, get the content, replace the content, and 
+# write to new output location
+foreach ($File in $FileListSolution) {
+    $OutputFile = $File.FullName;
+	
+    $Content = (Get-Content -Path $File.FullName -Raw).Replace("DS_PLATFORM_086", "DS_PLATFORM_090").
+		Replace("CINDER_086", "CINDER_090");
+	
+    Set-Content -Path $OutputFile -Value $Content;
+}
+
+
+Write-Host "Finished!";
 
 pause
