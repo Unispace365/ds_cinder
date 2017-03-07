d23678350a33308c33b5336dbae27e592b5b4b5b
Gordon Nickel <gordon.nickel@downstream.com>
2017-03-06 16:02:07 -0800

    IpCircleMask: add params to add a pad on the circle (make it smaller)

--- src/ds/ui/ip/functions/ip_circle_mask.cpp
+++ src/ds/ui/ip/functions/ip_circle_mask.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 
+#include "ds/util/string_util.h"
 #include <ds/ui/ip/functions/ip_circle_mask.h>
 
 namespace ds {
@@ -18,14 +19,27 @@ void CircleMask::on(const std::string& parameters, ci::Surface8u& s) const {
 	if(w < 1 || h < 1) return;
 
 	const glm::vec2			cen(static_cast<float>(w) / 2.0f, static_cast<float>(h) / 2.0f);
-	const float				max = (cen.x <= cen.y ? cen.x : cen.y);
+	float				max = (cen.x <= cen.y ? cen.x : cen.y);
+
+	if(!parameters.empty()){
+		if(parameters.find("pad:") != std::string::npos){
+			float pad = ds::string_to_float(parameters.substr(4));
+			if(pad > max){
+				max = 0.0f;
+			} else {
+				max = max - pad;
+			}
+		}
+	}
+
+	max = max * max; // compare the squared distance for speed
 
 	ci::Surface8u::Iter		iter = s.getIter();
 	int32_t					y = 0;
 	while(iter.line()) {
 		int32_t				x = 0;
 		while(iter.pixel()) {
-			const float		d = glm::distance(cen, glm::vec2(static_cast<float>(x), static_cast<float>(y)));
+			const float		d = glm::distance2(cen, glm::vec2(static_cast<float>(x), static_cast<float>(y)));
 			float			alpha_f = 1.0f;
 			if(d > max) {
 				alpha_f = 0.0f;
