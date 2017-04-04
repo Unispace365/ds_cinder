f3ce4508b7d9e89816d22c47a1a6ad958b95e516
Gordon Nickel <gordon.nickel@downstream.com>
2017-04-04 14:48:37 -0700

    Drop "unique" from unique id (and also the boost requirement there)

--- src/ds/app/engine/unique_id.cpp
+++ src/ds/app/engine/unique_id.cpp
@@ -1,27 +1,17 @@
-//#include "stdafx.h"
+#include "stdafx.h"
 
-// Turn off an unnecessary warning in the boost GUID
-#ifndef _SCL_SECURE_NO_WARNINGS
-#define _SCL_SECURE_NO_WARNINGS
-#endif
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
 
-#include <boost/asio.hpp>
-#include <boost/uuid/uuid.hpp>
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_io.hpp>
+#include <cinder/Rand.h>
+#include <string>
+#include <sstream>
 
 namespace ds {
 
 std::string					get_unique_id() {
 	std::stringstream		buf;
-	boost::uuids::uuid		uuid = boost::uuids::random_generator()();
-	// I stuff the machine name in there solely so it's easier to identify a
-	// problem machine when this gets used in an error report. It's entirely
-	// possible that in practice, the machine name is the only thing needed,
-	// but we'll play it safe.
-	buf << boost::asio::ip::host_name() << "-" << uuid;
+	auto compName = getenv("COMPUTERNAME");
+	ci::Rand::randSeed((uint32_t)std::time(0));
+	buf << compName << "-" << ci::randInt();
 	return buf.str();
 }
-
 } // namespace ds
