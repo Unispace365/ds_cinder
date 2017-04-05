a8f32ab1305bce513f10982d1ce0f1c8c1acde19
Nick Hogle <info@nickhogle.com>
2017-04-05 15:28:37 -0700

    Fix cinder GLSL vertex attribute warnings for Border sprites

--- src/ds/ui/sprite/border.cpp
+++ src/ds/ui/sprite/border.cpp
@@ -102,7 +102,7 @@ void Border::onBuildRenderBatch() {
 	const float h = getHeight();
 	auto rect = ci::Rectf(0.0f, 0.0f, w, h);
 	if(mCornerRadius > 0.0f){
-		auto theGeom = ci::geom::WireRoundedRect(rect, mCornerRadius);
+		auto theGeom = ci::geom::RoundedRect(rect, mCornerRadius);
 		if(mRenderBatch){
 			mRenderBatch->replaceVboMesh(ci::gl::VboMesh::create(theGeom));
 		} else {
