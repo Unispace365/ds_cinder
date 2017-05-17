a1e7994a97b8d76355945ac6c6c13de28415efac
Nick Hogle <info@nickhogle.com>
2017-05-16 18:13:35 -0700

    Linux build: Fix blocking call to std::async call in MQTT watcher

--- projects/mosquitto/src/ds/network/mqtt/mqtt_watcher.cpp
+++ projects/mosquitto/src/ds/network/mqtt/mqtt_watcher.cpp
@@ -12,6 +12,22 @@
 namespace ds {
 namespace net {
 
+// From http://stackoverflow.com/questions/16296284/workaround-for-blocking-async
+// Visual Studio incorrectly returns a future with a non-blocking destructor.
+// This workaround returns a non-blocking destructor on both Linux (GCC) and Windows (Visual Studio)
+namespace async_workaround {
+template< class Function, class... Args>
+std::future<typename std::result_of<Function(Args...)>::type> async( Function&& f, Args&&... args ) {
+	typedef typename std::result_of<Function(Args...)>::type R;
+	auto bound_task = std::bind(std::forward<Function>(f), std::forward<Args>(args)...);
+	std::packaged_task<R()> task(std::move(bound_task));
+	auto ret = task.get_future();
+	std::thread t(std::move(task));
+	t.detach();
+	return ret;
+}
+} //!namespace async_workaround
+
 namespace {
 
 //for initialization and convenience
@@ -104,7 +120,7 @@ void MqttWatcher::startListening(){
 	mStarted = true;
 	if(!mLoop.mConnected){
 		DS_LOG_INFO_M("Attempting to connect to the MQTT server...", MQTT_LOG);
-		std::async(std::launch::async, [this]{ mLoop.run(); });
+		async_workaround::async([this]{ mLoop.run(); });
 		mLoop.mConnected = true;
 	}
 }
