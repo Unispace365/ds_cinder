de1655bca9456f82c0c0b1d603b0d942b206a574
Gordon Nickel <gordon.nickel@downstream.com>
2017-06-29 18:39:56 -0700

    Start adding a data and query example

---
+++ example/data_and_query/src/app/data_and_query.h
@@ -0,0 +1,52 @@
+#ifndef _FULLSTARTER_APP_H_
+#define _FULLSTARTER_APP_H_
+
+#include <cinder/app/App.h>
+#include <ds/app/app.h>
+#include <ds/app/event_client.h>
+#include <ds/touch/touch_debug.h>
+
+#include "app/globals.h"
+#include "query/query_handler.h"
+
+namespace fullstarter {
+class AllData;
+
+class QueryAndDataApp : public ds::App {
+public:
+	QueryAndDataApp();
+
+	virtual void		mouseDown(ci::app::MouseEvent e);
+	virtual void		mouseDrag(ci::app::MouseEvent e);
+	virtual void		mouseUp(ci::app::MouseEvent e);
+	virtual void		keyDown(ci::app::KeyEvent event);
+	void				setupServer();
+	void				update();
+
+	virtual void		fileDrop(ci::app::FileDropEvent event);
+
+private:
+
+	void				forceStartIdleMode();
+	void				onAppEvent(const ds::Event&);
+
+	// Data
+	AllData				mAllData;
+
+	// Data acquisition
+	Globals				mGlobals;
+	QueryHandler		mQueryHandler;
+
+	//Idle state of the app to detect state change
+	bool				mIdling;
+
+	// Handle mouse events and simulate touch events
+	ds::TouchDebug		mTouchDebug;
+
+	// App events can be handled here
+	ds::EventClient		mEventClient;
+};
+
+} // !namespace fullstarter
+
+#endif // !_FULLSTARTER_APP_H_
