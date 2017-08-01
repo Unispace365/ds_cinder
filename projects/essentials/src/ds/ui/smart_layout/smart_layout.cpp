56549cbdcec3232b1c4c91945047fa95c24d360b
s133p <darkchild50@gmail.com>
2017-08-01 11:30:56 -0700

    First working smart_layout implementation

--- projects/essentials/src/ds/ui/smart_layout/smart_layout.cpp
+++ projects/essentials/src/ds/ui/smart_layout/smart_layout.cpp
@@ -1,162 +1,124 @@
 #include "stdafx.h"
 
-#include "layout_sprite.h"
+#include "smart_layout.h"
 
 #include <ds/app/environment.h>
-#include <ds/ui/sprite/sprite_engine.h>
 #include <ds/debug/logger.h>
-#include <ds/ui/sprite/text_pango.h>
+#include <ds/ui/interface_xml/interface_xml_importer.h>
 #include <ds/ui/sprite/multiline_text.h>
+#include <ds/ui/sprite/sprite_engine.h>
+#include <ds/ui/sprite/text_pango.h>
 #include <ds/util/string_util.h>
 
 
 namespace ds {
 namespace ui {
 
-SmartLayout::SmartLayout(Globals& g, std::string xmlLayoutFile)
-  : ds::ui::Sprite(g.mEngine)
-  , mGlobals(g)
+SmartLayout::SmartLayout(ds::ui::SpriteEngine& engine, std::string xmlLayoutFile)
+  : ds::ui::LayoutSprite(engine)
   , mLayoutFile(xmlLayoutFile)
-  , mRootLayout(nullptr)
-  , mEventClient(g.mEngine.getNotifier(), [this](const ds::Event* m) {
-      if (m) this->onAppEvent(*m);
+  , mEventClient(engine.getNotifier(), [this](const ds::Event* m) {
+	  if (m) this->onAppEvent(*m);
   }) {
 
-    ds::ui::XmlImporter::loadXMLto(this, ds::Environment::expand("%APP%/data/layouts/" + mLayoutFile), mSpriteMap);
-    mRootLayout = dynamic_cast<ds::ui::LayoutSprite*>(mSpriteMap["root"]);
+	// Fit all children of the XML layout. Not totally convinced about this
+	setLayoutType(LayoutType::kLayoutVFlow);
+	setShrinkToChildren(ShrinkType::kShrinkBoth);
 
-    runLayout(true);
+	ds::ui::XmlImporter::loadXMLto(this, ds::Environment::expand("%APP%/data/layouts/" + mLayoutFile), mSpriteMap);
+
+	runLayout();
 }
 
 void SmartLayout::onAppEvent(const ds::Event& in_e) {
-    if (mEventCallbacks.empty()) return;
+	if (mEventCallbacks.empty()) return;
 
-    auto callbackIt = mEventCallbacks.find(in_e.mWhat);
-    if (callbackIt != end(mEventCallbacks)) {
-        (callbackIt->second)(in_e);
-    }
+	auto callbackIt = mEventCallbacks.find(in_e.mWhat);
+	if (callbackIt != end(mEventCallbacks)) {
+		(callbackIt->second)(in_e);
+	}
 }
 
-void SmartLayout::setTextSprite(std::string spriteName, std::string value) {
-    return setTextSprite(spriteName, ds::wstr_from_utf8(value));
+void SmartLayout::listen(size_t type, std::function<void(const ds::Event&)> callback) {
+	mEventCallbacks[type] = callback;
 }
 
-void SmartLayout::setTextSprite(std::string spriteName, std::wstring value) {
-    ds::ui::Text*          spr  = dynamic_cast<ds::ui::Text*>(mSpriteMap[spriteName]);
-    ds::ui::MultilineText* sprM = dynamic_cast<ds::ui::MultilineText*>(mSpriteMap[spriteName]);
+void SmartLayout::setSpriteText(std::string spriteName, std::string value) {
+	return setSpriteText(spriteName, ds::wstr_from_utf8(value));
+}
 
-    if (spr) {
-        spr->setText(value);
-    } else if (sprM) {
-        sprM->setText(value);
-    } else {
-        DS_LOG_WARNING("Failed to set Text for Sprite: " << spriteName);
-        return *this;
-    }
+void SmartLayout::setSpriteText(std::string spriteName, std::wstring value) {
+	ds::ui::Text*		   spr  = dynamic_cast<ds::ui::Text*>(mSpriteMap[spriteName]);
+	ds::ui::MultilineText* sprM = dynamic_cast<ds::ui::MultilineText*>(mSpriteMap[spriteName]);
 
-    runLayout(true);
-    return *this;
+	if (spr) {
+		spr->setText(value);
+	} else if (sprM) {
+		sprM->setText(value);
+	} else {
+		DS_LOG_WARNING("Failed to set Text for Sprite: " << spriteName);
+	}
+
+	runLayout();
 }
 
 void SmartLayout::setSpriteFont(std::string spriteName, std::string value) {
-    ds::ui::Text*          spr  = dynamic_cast<ds::ui::Text*>(mSpriteMap[spriteName]);
-    ds::ui::MultilineText* sprM = dynamic_cast<ds::ui::MultilineText*>(mSpriteMap[spriteName]);
+	ds::ui::Text*		   spr  = dynamic_cast<ds::ui::Text*>(mSpriteMap[spriteName]);
+	ds::ui::MultilineText* sprM = dynamic_cast<ds::ui::MultilineText*>(mSpriteMap[spriteName]);
 
-    if (spr) {
-        mGlobals.mEngine.getEngineCfg().getText(value).configure(*spr);
-    } else if (sprM) {
-        mGlobals.mEngine.getEngineCfg().getText(value).configure(*sprM);
-    } else {
-        DS_LOG_WARNING("Failed to set Font " << value << " for Sprite: " << spriteName);
-        return *this;
-    }
+	if (spr) {
+		mEngine.getEngineCfg().getText(value).configure(*spr);
+	} else if (sprM) {
+		mEngine.getEngineCfg().getText(value).configure(*sprM);
+	} else {
+		DS_LOG_WARNING("Failed to set Font " << value << " for Sprite: " << spriteName);
+	}
 
-    runLayout(true);
-    return *this;
+	runLayout();
 }
 
-void SmartLayout::setImageSprite(std::string spriteName, std::string value) {
-    ds::ui::Image* sprI = dynamic_cast<ds::ui::Image*>(mSpriteMap[spriteName]);
+void SmartLayout::setSpriteImage(std::string spriteName, std::string value) {
+	ds::ui::Image* sprI = dynamic_cast<ds::ui::Image*>(mSpriteMap[spriteName]);
 
-    if (sprI) {
-        sprI->setImageFile(ds::Environment::expand("%APP%/data/images/" + value));
-    } else {
-        DS_LOG_WARNING("Failed to set Image for Sprite: " << spriteName);
-        return *this;
-    }
+	if (sprI) {
+		sprI->setImageFile(ds::Environment::expand("%APP%/data/images/" + value));
+	} else {
+		DS_LOG_WARNING("Failed to set Image for Sprite: " << spriteName);
+	}
 
-    runLayout(true);
-    return *this;
+	runLayout();
 }
 
-void SmartLayout::setImageSprite(std::string spriteName, ds::Resource value) {
-    ds::ui::Image* sprI = dynamic_cast<ds::ui::Image*>(mSpriteMap[spriteName]);
+void SmartLayout::setSpriteImage(std::string spriteName, ds::Resource value) {
+	ds::ui::Image* sprI = dynamic_cast<ds::ui::Image*>(mSpriteMap[spriteName]);
 
-    if (sprI) {
-        sprI->setImageResource(value);
-    } else {
-        DS_LOG_WARNING("Failed to set Image for Sprite: " << spriteName);
-        return *this;
-    }
+	if (sprI) {
+		sprI->setImageResource(value);
+	} else {
+		DS_LOG_WARNING("Failed to set Image for Sprite: " << spriteName);
+	}
 
-    runLayout(true);
-    return *this;
-}
-
-void SmartLayout::setVideoSprite(std::string spriteName, std::string value) {
-    ds::ui::Video* sprI = dynamic_cast<ds::ui::Video*>(mSpriteMap[spriteName]);
-
-    if (sprI) {
-        sprI->loadVideo(ds::Environment::expand("%APP%/data/videos/" + value));
-    } else {
-        DS_LOG_WARNING("Failed to set Video for Sprite: " << spriteName);
-        return *this;
-    }
-
-    runLayout(true);
-    return *this;
-}
-
-void SmartLayout::setVideoSprite(std::string spriteName, ds::Resource value) {
-    ds::ui::Video* sprI = dynamic_cast<ds::ui::Video*>(mSpriteMap[spriteName]);
-
-    if (sprI) {
-        sprI->setResource(value);
-    } else {
-        DS_LOG_WARNING("Failed to set Video for Sprite: " << spriteName);
-        return *this;
-    }
-
-    runLayout(true);
-    return *this;
-}
-
-void SmartLayout::listen(int type, std::function<void(const ds::Event&)> callback) {
-    mEventCallbacks[type] = callback;
-    return *this;
+	runLayout();
 }
 
 void SmartLayout::setSpriteTapFn(std::string spriteName,
-                                         const std::function<void(ds::ui::Sprite*, const ci::vec3&)>& tapCallback) {
-    ds::ui::Sprite* spr = dynamic_cast<ds::ui::Sprite*>(mSpriteMap[spriteName]);
-    if (spr && tapCallback) {
-        spr->enable(true);
-        spr->setTapCallback(tapCallback);
-    }
-    return *this;
+								 const std::function<void(ds::ui::Sprite*, const ci::vec3&)>& tapCallback) {
+	ds::ui::Sprite* spr = dynamic_cast<ds::ui::Sprite*>(mSpriteMap[spriteName]);
+	if (spr && tapCallback) {
+		spr->enable(true);
+		spr->setTapCallback(tapCallback);
+	}
 }
 
 void SmartLayout::addSpriteChild(std::string spriteName, ds::ui::Sprite* newChild) {
-    ds::ui::Sprite* spr = dynamic_cast<ds::ui::Sprite*>(mSpriteMap[spriteName]);
-    if (spr && newChild) {
-        spr->addChildPtr(newChild);
-    } else {
-        DS_LOG_WARNING("Failed to add child to " << spriteName);
-    }
-    return *this;
+	ds::ui::Sprite* spr = dynamic_cast<ds::ui::Sprite*>(mSpriteMap[spriteName]);
+	if (spr && newChild) {
+		spr->addChildPtr(newChild);
+	} else {
+		DS_LOG_WARNING("Failed to add child to " << spriteName);
+	}
 }
 
 
-
-} // namespace ui
-} // namespace ds
+}  // namespace ui
+}  // namespace ds
--- projects/essentials/src/ds/ui/smart_layout/smart_layout.h
+++ projects/essentials/src/ds/ui/smart_layout/smart_layout.h
@@ -1,8 +1,9 @@
 #pragma once
-#ifndef DS_UI_LAYOUT_LAYOUT_SPRITE
-#define DS_UI_LAYOUT_LAYOUT_SPRITE
+#ifndef DS_UI_LAYOUT_SMART_LAYOUT_SPRITE
+#define DS_UI_LAYOUT_SMART_LAYOUT_SPRITE
 
 
+#include <ds/ui/layout/layout_sprite.h>
 #include <ds/ui/sprite/sprite.h>
 #include <ds/ui/sprite/sprite_engine.h>
 
@@ -13,53 +14,62 @@ namespace ui {
 * \class ds::ui::SmartLayout
 *        SmartLayout combines a layout sprite with XML importing.
 */
-class SmartLayout : public ds::ui::LayoutSprite  {
-public:
-    SmartLayout(ds::ui::SpriteEngine& engine, std::string xmlLayoutFile);
+class SmartLayout : public ds::ui::LayoutSprite {
+  public:
+	SmartLayout(ds::ui::SpriteEngine& engine, std::string xmlLayoutFile);
 
-    bool hasSprite(std::string spriteName) { return mSpriteMap[spriteName] != nullptr; }
+	bool hasSprite(std::string spriteName) {
+		return mSpriteMap[spriteName] != nullptr;
+	}
 
-    ds::ui::Sprite* getSprite(std::string spriteName) { return mSpriteMap.at(spriteName); }
-    template <typename T>
-    T* getSprite(std::string spriteName) { return dynamic_cast<T*>(mSpriteMap.at(spriteName);); }
+	ds::ui::Sprite* getSprite(std::string spriteName) {
+		return mSpriteMap.at(spriteName);
+	}
+	template <typename T>
+	T* getSprite(std::string spriteName) {
+		return dynamic_cast<T*>(mSpriteMap.at(spriteName););
+	}
 
-    void addSpriteChild(std::string spriteName, ds::ui::Sprite* newChild);
+	void addSpriteChild(std::string spriteName, ds::ui::Sprite* newChild);
 
-    // Text sprite functions
-    void setSpriteText(std::string, std::wstring);
-    void setSpriteText(std::string, std::string);
-    void setSpriteFont(std::string, std::string);
+	void listen(size_t type, std::function<void(const ds::Event&)> callback);
 
-    // Image sprite functions
-    void setSpriteImage(std::string, std::string);
-    void setSpriteImage(std::string, ds::Resource);
+	// Text sprite functions
+	void setSpriteText(std::string, std::wstring);
+	void setSpriteText(std::string, std::string);
+	void setSpriteFont(std::string, std::string);
 
-    void listen(size_t type, std::function<void(const ds::Event&)> callback);
-    void setSpriteTapFn(std::string spriteName, const std::function<void(ds::ui::Sprite*, const ci::vec3&)>& tapCallback);
+	// Image sprite functions
+	void setSpriteImage(std::string, std::string);
+	void setSpriteImage(std::string, ds::Resource);
 
-    // Build => Run Animations on children
-    // this->addAnimation(name, duration, delay).size(elementName, to, delay=0).opacity(elementName, to,  delay=0).finishFn(callback);
-    // this->addAnimationScript(name, file-or-string)
-    // this->runAnimation(name)
+	void setSpriteTapFn(std::string spriteName,
+						const std::function<void(ds::ui::Sprite*, const ci::vec3&)>& tapCallback);
 
-    // Run Animations on children immediately
-    // this->animate(duration, delay)
-    // this->animate(file-or-string)
+	// Build => Run Animations on children
+	// this->addAnimation(name, duration, delay).size(elementName, to, delay=0).opacity(elementName, to,
+	// delay=0).finishFn(callback);
+	// this->addAnimationScript(name, file-or-string)
+	// this->runAnimation(name)
 
-protected:
-    using sMap          = std::map<std::string, ds::ui::Sprite*>;
-    using eventCallback = std::function<void(const ds::Event&)>;
-    using eventMap      = std::unordered_map<int, eventCallback>;
+	// Run Animations on children immediately
+	// this->animate(duration, delay)
+	// this->animate(file-or-string)
 
-    void onAppEvent(const ds::Event&);
+  protected:
+	using sMap			= std::map<std::string, ds::ui::Sprite*>;
+	using eventCallback = std::function<void(const ds::Event&)>;
+	using eventMap		= std::unordered_map<int, eventCallback>;
 
-    std::string           mLayoutFile;
-    ds::EventClient       mEventClient;
-    sMap                  mSpriteMap;
-    eventMap              mEventCallbacks;
+	void onAppEvent(const ds::Event&);
+
+	std::string		mLayoutFile;
+	ds::EventClient mEventClient;
+	sMap			mSpriteMap;
+	eventMap		mEventCallbacks;
 };
 
-} // namespace ui
-} // namespace ds
+}  // namespace ui
+}  // namespace ds
 
 #endif
