55d354c6b6bde42c35fa498c5e483c185b6d5ba9
s133p <darkchild50@gmail.com>
2017-11-09 13:14:43 -0800

    Fix non-compiling commit!

--- src/ds/ui/sprite/text.cpp
+++ src/ds/ui/sprite/text.cpp
@@ -693,7 +693,7 @@ bool Text::measurePangoText() {
 			if (attrs == nullptr) { attrs = pango_attr_list_new(); }
 
 			// Set letter spacing: 0.0f=normal; 1.0f = 1pt extra spacing;
-			pango_attr_list_insert(attrs, pango_attr_letter_spacing_new((int)(mLetterSpacing * 1024.0f));
+			pango_attr_list_insert(attrs, pango_attr_letter_spacing_new((int)(mLetterSpacing * 1024.0f)));
 
 			// Enable ligatures, kerning, and auto-conversion of simple fractions to a single character representation
 			pango_attr_list_insert(attrs, pango_attr_font_features_new("liga=1, -kern, afrc on, frac on"));
