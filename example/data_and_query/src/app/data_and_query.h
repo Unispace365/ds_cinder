5e288e211f5e9e8fd65a718185b98df9040d145b
Gordon Nickel <gordon.nickel@downstream.com>
2018-02-19 16:45:39 -0800

    Example: Update data_and_query

--- example/data_and_query/src/app/data_and_query.h
+++ example/data_and_query/src/app/data_and_query.h
@@ -4,7 +4,6 @@
 #include <cinder/app/App.h>
 #include <ds/app/app.h>
 #include <ds/app/event_client.h>
-#include <ds/touch/touch_debug.h>
 
 #include "app/globals.h"
 #include "query/query_handler.h"
@@ -16,9 +15,6 @@ class QueryAndDataApp : public ds::App {
 public:
 	QueryAndDataApp();
 
-	virtual void		mouseDown(ci::app::MouseEvent e);
-	virtual void		mouseDrag(ci::app::MouseEvent e);
-	virtual void		mouseUp(ci::app::MouseEvent e);
 	virtual void		onKeyDown(ci::app::KeyEvent event) override;
 	void				setupServer();
 	void				update();
@@ -26,8 +22,6 @@ public:
 	virtual void		fileDrop(ci::app::FileDropEvent event);
 
 private:
-
-	void				forceStartIdleMode();
 	void				onAppEvent(const ds::Event&);
 
 	// Data
@@ -36,12 +30,6 @@ private:
 	// Data acquisition
 	Globals				mGlobals;
 	QueryHandler		mQueryHandler;
-
-	//Idle state of the app to detect state change
-	bool				mIdling;
-
-	// Handle mouse events and simulate touch events
-	ds::TouchDebug		mTouchDebug;
 
 	// App events can be handled here
 	ds::EventClient		mEventClient;
