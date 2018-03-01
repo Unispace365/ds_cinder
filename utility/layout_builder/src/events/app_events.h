facea62470837e5e4da561d2c175ac743187fdbf
Gordon Nickel <gordon.nickel@downstream.com>
2018-02-28 16:10:08 -0800

    LayoutBuilder: move to utility folder

---
+++ utility/layout_builder/src/events/app_events.h
@@ -0,0 +1,114 @@
+#ifndef _LAYOUT_BUILDER_APP_APPEVENTS_H_
+#define _LAYOUT_BUILDER_APP_APPEVENTS_H_
+
+#include <ds/app/event.h>
+#include <ds/ui/sprite/sprite.h>
+#include <ds/ui/sprite/text.h>
+
+namespace layout_builder {
+
+class IdleStartedEvent : public ds::RegisteredEvent < IdleStartedEvent > {
+public:
+	IdleStartedEvent(){};
+};
+
+class IdleEndedEvent : public ds::RegisteredEvent < IdleEndedEvent > {
+public:
+	IdleEndedEvent(){};
+
+};
+
+class LoadLayoutRequest : public ds::RegisteredEvent<LoadLayoutRequest>{
+public:
+	LoadLayoutRequest(const std::string& layoutLocation) : mLocation(layoutLocation) {}
+	const std::string mLocation;
+};
+
+class AppRestartRequest : public ds::RegisteredEvent<AppRestartRequest>{
+public:
+	AppRestartRequest(){};
+};
+
+class RefreshLayoutRequest : public ds::RegisteredEvent<RefreshLayoutRequest>{
+public:
+	RefreshLayoutRequest(){};
+};
+
+class SaveLayoutRequest : public ds::RegisteredEvent<SaveLayoutRequest>{
+public:
+	SaveLayoutRequest(){};
+};
+
+class LayoutLayoutRequest : public ds::RegisteredEvent<LayoutLayoutRequest>{
+public:
+	LayoutLayoutRequest(){};
+};
+
+class AnimateLayoutRequest : public ds::RegisteredEvent<AnimateLayoutRequest>{
+public:
+	AnimateLayoutRequest(){};
+};
+
+class InspectTreeRequest : public ds::RegisteredEvent<InspectTreeRequest>{
+public:
+	// ownership of the tree doesn't change
+	InspectTreeRequest(ds::ui::Sprite* sp) :mTree(sp){}
+	ds::ui::Sprite*		mTree;
+};
+
+class InspectSpriteRequest : public ds::RegisteredEvent<InspectSpriteRequest>{
+public:
+	// ownership of the tree doesn't change
+	InspectSpriteRequest(ds::ui::Sprite* sp) :mSprid(sp){}
+	ds::ui::Sprite*		mSprid;
+};
+
+class InputFieldSetRequest : public ds::RegisteredEvent<InputFieldSetRequest>{
+public:
+	// sets the text sprite to capute text input
+	// send nullptr to clear the input field
+	InputFieldSetRequest(ds::ui::Text* inputField) : mInputField(inputField){}
+	ds::ui::Text*		mInputField;
+};
+
+class InputFieldCleared : public ds::RegisteredEvent<InputFieldCleared>{
+public:
+	InputFieldCleared(){}
+};
+
+class InputFieldTextInput : public ds::RegisteredEvent<InputFieldTextInput>{
+public:
+	InputFieldTextInput(ds::ui::Text* inputField, const std::wstring fullText) : mInputField(inputField), mFullText(fullText){}
+	ds::ui::Text*		mInputField;
+	const std::wstring	mFullText;
+
+};
+
+class CreateSpriteRequest : public ds::RegisteredEvent<CreateSpriteRequest>{
+public:
+	CreateSpriteRequest(const std::string& typeName) : mTypeName(typeName){}
+	const std::string	mTypeName;
+};
+
+class DeleteSpriteRequest : public ds::RegisteredEvent<DeleteSpriteRequest>{
+public:
+	DeleteSpriteRequest(ds::ui::Sprite* spriteToDelete) : mSpriteToDelete(spriteToDelete){}
+	ds::ui::Sprite*		mSpriteToDelete;
+};
+
+class MouseMoveEvent : public ds::RegisteredEvent<MouseMoveEvent>{
+public:
+	MouseMoveEvent( const ci::vec3 mousePoint ) : mMousePoint(mousePoint) {}
+	ci::vec3			mMousePoint;
+};
+
+class ShowSpriteHighlightEvent : public ds::RegisteredEvent<ShowSpriteHighlightEvent>{
+public:
+	ShowSpriteHighlightEvent( ds::ui::Sprite* spriteToHighlight ) : mSpriteToHighlight( spriteToHighlight ){}
+	ds::ui::Sprite*		mSpriteToHighlight;
+};
+
+
+} // !namespace layout_builder
+
+#endif // !_LAYOUT_BUILDER_APP_APPEVENTS_H_
---
+++ utility/layout_builder/src/ui/tree_item.h
@@ -0,0 +1,40 @@
+#pragma once
+#ifndef LAYOUT_BUILDER_UI_TREE_ITEM
+#define LAYOUT_BUILDER_UI_TREE_ITEM
+
+
+#include <ds/ui/sprite/sprite.h>
+#include <ds/ui/sprite/text.h>
+
+namespace layout_builder {
+
+class Globals;
+
+/**
+* \class layout_builder::TreeItem
+*			A single item in a tree Hierarchy
+*/
+class TreeItem : public ds::ui::Sprite  {
+public:
+	TreeItem(Globals& g, ds::ui::Sprite* linkedItem); 
+
+	ds::ui::Sprite*						getLinkedSprite(){ return mLinkedSprite; }
+	std::string							getPropertyName(){ if(mNameText) return mNameText->getTextAsString(); return ""; }
+	ds::ui::Text*						getValueField(){ return mLabelTextTwo; }
+	void								setValueText(const std::wstring& labelTwoText);
+
+private:
+
+	Globals&							mGlobals;
+	ds::ui::Text*						mNameText;
+	ds::ui::Text*						mLabelTextTwo;
+	ds::ui::Sprite*						mLinkedSprite;
+
+
+};
+
+} // namespace layout_builder
+
+#endif
+
+
