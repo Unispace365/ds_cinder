3e615273f45cdcf4211ad9d8dc1887fc65e44baf
Gordon Nickel <gordon.nickel@downstream.com>
2018-03-06 11:12:27 -0800

    Install: start adding installer scripts

---
+++ example/full_starter/install/download_dsnode.ps1
@@ -0,0 +1,27 @@
+﻿Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$DsnodeUrl = "http://update.downstreamdev.com/ds/DsNode/dsnode.zip";
+$ThisCommand = $MyInvocation.MyCommand.Path;
+$ThisDir = (Get-Item $ThisCommand ).Directory.FullName;
+$TempFile = $ThisDir + "/dsnoder.zip";
+$DsnodeDir = $ThisDir + "/DSNode/";
+
+if (Test-Path $DsnodeDir -PathType Container){
+    Write-Host Removing old dsnode dir...;
+    Remove-Item $DsnodeDir -Recurse;
+}
+
+Write-Host Downloading dsnode zip...;
+Invoke-WebRequest -Uri $DsnodeUrl -OutFile $TempFile;
+
+Write-Host Extracting dsnode zip...
+[System.IO.Compression.ZipFile]::ExtractToDirectory($TempFile, $DsnodeDir);
+
+Write-Host Removing zip file...;
+Remove-Item $TempFile;
+
+if (Test-Path $DsnodeDir -PathType Container){
+    Write-Host Success!;
+} else {
+    Write-Host Uh oh, something went wrong!;
+}
