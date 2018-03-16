24efef3ed1bff24623a495cde5d844e2610d6073
Gordon Nickel <gordon.nickel@downstream.com>
2018-03-16 14:50:24 -0700

    Install: update dsnode and apphost downloaders

--- example/getting_started/install/download_dsnode.ps1
+++ example/getting_started/install/download_dsnode.ps1
@@ -1,10 +1,15 @@
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-$DsnodeUrl = "http://update.downstreamdev.com/ds/DsNode/dsnode.zip";
+$DsnodeVer = (Invoke-WebRequest -Uri 'http://update.downstreamdev.com/ds/dsnode/version.txt').Content;
+Write-Host $DsnodeVer;
+
+
+$DsnodeUrl = "http://update.downstreamdev.com/ds/dsnode/" + $DsnodeVer;
 $ThisCommand = $MyInvocation.MyCommand.Path;
 $ThisDir = (Get-Item $ThisCommand ).Directory.FullName;
 $TempFile = $ThisDir + "/dsnoder.zip";
 $DsnodeDir = $ThisDir + "/DSNode/";
+$OutputDir = $ThisDir;
 
 if (Test-Path $DsnodeDir -PathType Container){
     Write-Host Removing old dsnode dir...;
@@ -15,7 +20,7 @@ Write-Host Downloading dsnode zip...;
 Invoke-WebRequest -Uri $DsnodeUrl -OutFile $TempFile;
 
 Write-Host Extracting dsnode zip...
-[System.IO.Compression.ZipFile]::ExtractToDirectory($TempFile, $DsnodeDir);
+[System.IO.Compression.ZipFile]::ExtractToDirectory($TempFile, $OutputDir);
 
 Write-Host Removing zip file...;
 Remove-Item $TempFile;
