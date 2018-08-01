e5bc197a14215b53a278dfc7a79a917fc4b97acd
Gordon Nickel <gordon.nickel@downstream.com>
2018-07-31 17:09:33 -0700

    MediaPlayer: add setting for video loop

--- projects/viewers/src/ds/ui/media/media_player.cpp
+++ projects/viewers/src/ds/ui/media/media_player.cpp
@@ -127,6 +127,20 @@ public:
 				mediaPlayer->setSettings(mvs);
 
 			});
+
+			e.registerSpritePropertySetter("media_player_video_loop", [](ds::ui::Sprite& theSprite, const std::string& theValue, const std::string& fileReferrer) {
+
+				ds::ui::MediaPlayer* mediaPlayer = dynamic_cast<ds::ui::MediaPlayer*>(&theSprite);
+				if(!mediaPlayer) {
+					DS_LOG_WARNING("Tried to set the property media_player_video_loop on a non-mediaPlayer sprite");
+					return;
+				}
+
+				auto& mvs = mediaPlayer->getSettings();
+				mvs.mVideoLoop = ds::parseBoolean(theValue);
+				mediaPlayer->setSettings(mvs);
+
+			});
 		});
 	}
 
