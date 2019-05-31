26cb46e06b5df1b7ec6cfda979b40f36eb304152
Gordon Nickel <gordon.nickel@downstream.com>
2019-05-31 13:14:04 -0700

    XmlImporter: specify file when warning about dupe sprite names

--- projects/essentials/src/ds/ui/interface_xml/interface_xml_importer.cpp
+++ projects/essentials/src/ds/ui/interface_xml/interface_xml_importer.cpp
@@ -1724,7 +1724,7 @@ bool XmlImporter::readSprite(ds::ui::Sprite* parent, std::unique_ptr<ci::XmlTree
 			spriddy->setSpriteName(ds::wstr_from_utf8(sprite_name));
 
 			if (mNamedSpriteMap.find(sprite_name) != mNamedSpriteMap.end()) {
-				DS_LOG_WARNING("Interface xml file contains duplicate sprites named:"
+ 				DS_LOG_WARNING("Interface xml file " << mXmlFile <<" contains duplicate sprites named:"
 							   << sprite_name << ", only the first one will be identified.");
 			} else {
 				mNamedSpriteMap.insert(std::make_pair(sprite_name, spriddy));
