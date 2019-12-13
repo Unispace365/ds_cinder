0da31dbf0a602dbbf2959ca41fcfbb6201ff7bc3
Gordon Nickel <gordon.nickel@downstream.com>
2019-12-13 08:37:55 -0800

    Add an auto-slideshow utility app

---
+++ utility/auto_slideshow/install/download_apphost.ps1
@@ -0,0 +1,34 @@
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+$DsapphostVer = (Invoke-WebRequest -UseBasicParsing -Uri 'http://update.downstreamdev.com/ds/dsapphost/version.txt').Content;
+Write-Host $DsapphostVer;
+
+
+$DsapphostUrl = "http://update.downstreamdev.com/ds/dsapphost/" + $DsapphostVer;
+$ThisCommand = $MyInvocation.MyCommand.Path;
+$ThisDir = (Get-Item $ThisCommand ).Directory.FullName;
+$TempFile = $ThisDir + "/Dsapphostr.zip";
+$DsapphostDir = $ThisDir + "/DSAppHost/";
+$OutputDir = $ThisDir;
+
+if (Test-Path $DsapphostDir -PathType Container){
+    Write-Host Removing old Dsapphost dir...;
+    Remove-Item $DsapphostDir -Recurse;
+}
+
+Write-Host Downloading Dsapphost zip...;
+Invoke-WebRequest -Uri $DsapphostUrl -OutFile $TempFile;
+
+Write-Host Extracting Dsapphost zip...
+[System.IO.Compression.ZipFile]::ExtractToDirectory($TempFile, $OutputDir);
+
+Write-Host Removing zip file...;
+Remove-Item $TempFile;
+
+if (Test-Path $DsapphostDir -PathType Container){
+    Write-Host Success!;
+} else {
+    Write-Host Uh oh, something went wrong!;
+}
+
+
