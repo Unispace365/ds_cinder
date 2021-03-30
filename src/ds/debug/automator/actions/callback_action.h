92fc26b48922743b52970b6630786b2b09e78290
Aubrey Francois <aubrey@aubreyfrancois.com>
2021-03-30 12:32:13 -0700

    Update src/ds/debug director to add missing files

---
+++ src/ds/debug/automator/actions/callback_action.h
@@ -0,0 +1,55 @@
+#pragma once
+#ifndef ESSENTIALS_DS_DEBUG_AUTOMATOR_ACTIONS_CALLBACK_ACTION_H_
+#define ESSENTIALS_DS_DEBUG_AUTOMATOR_ACTIONS_CALLBACK_ACTION_H_
+
+#include "base_action.h"
+#include <cinder/Vector.h>
+
+namespace ds{
+namespace ui{
+class SpriteEngine;
+}
+namespace debug{
+
+/**
+* \class CallbackActionFactory
+*/
+class CallbackActionFactory : public BaseActionFactory {
+public:
+	CallbackActionFactory(std::function<void(void)> callback, const float minFrequency = 0.0f, const float maxFrequency = 1.0f)
+		:mCallback(callback)		
+		, mMinFrequency(minFrequency)
+		, mMaxFrequency(maxFrequency)
+	{}
+
+	virtual float			getLimit() const;
+	virtual int				getNumberOfFingers() const;
+
+	virtual BaseAction*		build(std::vector<int> &freeList, ds::ui::SpriteEngine& engine, const ci::Rectf& frame) const;
+
+	std::function<void(void)>	mCallback;
+	float						mMinFrequency;
+	float						mMaxFrequency;
+};
+
+/**
+* \class CallbackAction
+*/
+class CallbackAction : public BaseAction
+{
+public:
+	CallbackAction(std::vector<int> &freeList, ds::ui::SpriteEngine& engine, const ci::Rectf& frame, const std::function<void(void)>& callback, const float minTime, const float floatMaxTime);
+
+	void setup(float limit, int numberOfFingers);
+
+	virtual bool update(float dt);
+protected:
+	std::function<void(void)>	mCallback;
+	float						mMinTime;
+	float						mMaxTime;
+};
+
+} // namespace debug
+} // namespace ds
+
+#endif//DRAG_ACTION_DS_H
---
+++ src/ds/debug/automator/actions/tap_action.cpp
@@ -0,0 +1,74 @@
+#include "stdafx.h"
+
+#include "tap_action.h"
+
+#include <ds/ui/sprite/sprite_engine.h>
+#include <ds/ui/touch/touch_event.h>
+
+#include <cinder/Rand.h>
+#include <cinder/app/TouchEvent.h>
+
+namespace ds {
+namespace debug {
+
+/**
+ * \class TapActionFactory
+ */
+float TapActionFactory::getLimit() const {
+	return ci::randFloat(0.01f, 0.5f);
+}
+
+int TapActionFactory::getNumberOfFingers() const {
+	return 1;
+	// Our taps are considered 1-finger, so dunno why you would ever want a 0 or 4 finger tap since that wouldnt do shit
+	//	return ci::randInt(0, 5);
+}
+
+BaseAction* TapActionFactory::build(std::vector<int> &freeList, ds::ui::SpriteEngine& engine, const ci::Rectf& frame) const {
+	return new TapAction(freeList, engine, frame);
+}
+
+/**
+ * \class TapAction
+ */
+TapAction::TapAction(std::vector<int> &freeList, ds::ui::SpriteEngine& engine, const ci::Rectf& frame)
+	: BaseAction(freeList, engine, frame)
+{
+}
+
+TapAction::~TapAction(){
+}
+
+bool TapAction::update(float dt){
+	BaseAction::update(dt);
+
+	if(mTotal >= mLimit)
+	{
+		std::vector<ci::app::TouchEvent::Touch> touches;
+		for(int i = 0; i < mNumberOfFingers; ++i){
+			touches.push_back(ci::app::TouchEvent::Touch(mTouchPos[i], mTouchPos[i], mInUseList[i], dt, nullptr));
+		}
+		mEngine.injectTouchesEnded(ds::ui::TouchEvent(mEngine.getWindow(), touches, true));
+		return true;
+	}
+
+	return false;
+}
+
+void TapAction::setup(float limit, int numberOfFingers)
+{
+	BaseAction::setup(limit, numberOfFingers);
+
+	std::vector<ci::app::TouchEvent::Touch> touches;
+
+	mTouchPos.reserve(mInUseList.size());
+	for(auto it = mInUseList.begin(), it2 = mInUseList.end(); it != it2; ++it){
+		ci::vec2 touchPos = ci::vec2(mFrame.getX1() + ci::randFloat(0.0f, mFrame.getWidth()), mFrame.getY1() + ci::randFloat(0.0f, mFrame.getHeight()));
+		mTouchPos.push_back(touchPos);
+		touches.push_back(ci::app::TouchEvent::Touch(touchPos, touchPos, *it, 0.0, nullptr));
+	}
+	mEngine.injectTouchesBegin(ds::ui::TouchEvent(mEngine.getWindow(), touches, true));
+}
+
+} // namespace debug
+} // namespace ds
