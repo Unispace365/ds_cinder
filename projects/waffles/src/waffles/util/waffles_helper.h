0cb965f8fb17f1c2cc74361756f233875efa669a
Aubrey Francois <aubrey.francois@downstream.com>
2024-08-06 15:33:27 -0700

    Eric wanted this to be pretty.

--- projects/waffles/src/waffles/util/waffles_helper.h
+++ projects/waffles/src/waffles/util/waffles_helper.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <memory>
 #include <ds/app/event_client.h>
-#include <ds/ui/sprite/sprite.h>
 #include <ds/content/content_helper.h>
+#include <ds/ui/sprite/sprite.h>
+#include <memory>
 namespace waffles {
 
 /**
@@ -16,22 +16,16 @@ class WafflesHelper;
 typedef std::shared_ptr<WafflesHelper> WafflesHelperPtr;
 
 
-
-
-
 class WafflesHelper : public ds::model::ContentHelper {
   public:
-	  
 	WafflesHelper(ds::ui::SpriteEngine& eng)
-	  : ds::model::ContentHelper(eng) {};
-	
-	virtual bool							getApplyParticles() = 0;
-	virtual ds::model::ContentModelRef				getPinboard()= 0;
-	virtual ds::model::ContentModelRef				getAnnotationFolder()			   = 0;
-	virtual std::vector<ds::model::ContentModelRef> getValidPinboards()=0;
-	virtual std::vector<ds::Resource>				findMediaResources()=0;
+	  : ds::model::ContentHelper(eng){};
 
- 
+	virtual bool									getApplyParticles()	  = 0;
+	virtual ds::model::ContentModelRef				getPinboard()		  = 0;
+	virtual ds::model::ContentModelRef				getAnnotationFolder() = 0;
+	virtual std::vector<ds::model::ContentModelRef> getValidPinboards()	  = 0;
+	virtual std::vector<ds::Resource>				findMediaResources()  = 0;
 };
 
 } // namespace waffles
