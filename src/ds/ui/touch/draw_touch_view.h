79c1d6a557fcb075d9f0e17c005393315896d323
paulhoux <paulhoux@users.noreply.github.com>
2024-11-15 13:36:33 +0100

    DrawTouchView was creating sprites but never destroying them. Instead of using a newly created sprite for each finger id, we now simply keep a small list of finger instances and then draw all fingers using instanced rendering. This is both faster and more memory efficient, without causing memory leaks.

--- src/ds/ui/touch/draw_touch_view.h
+++ src/ds/ui/touch/draw_touch_view.h
@@ -2,36 +2,48 @@
 #ifndef DS_UI_TOUCH_DRAW_TOUCH_VIEW
 #define DS_UI_TOUCH_DRAW_TOUCH_VIEW
 
-#include <map>
-
 #include <ds/cfg/settings.h>
-#include <ds/data/data_buffer.h>
 #include <ds/ui/sprite/sprite.h>
 #include <ds/ui/sprite/sprite_engine.h>
 #include <ds/ui/touch/touch_manager.h>
-
-#include <cinder/gl/Vbo.h>
 
 namespace ds { namespace ui {
 	class Circle;
 
 	// View for drawing touches
-	class DrawTouchView : public ds::ui::Sprite, public ds::ui::TouchManager::Capture {
+	class DrawTouchView : public Sprite, public TouchManager::Capture {
 	  public:
-		DrawTouchView(ds::ui::SpriteEngine& e);
-		DrawTouchView(ds::ui::SpriteEngine& e, ds::cfg::Settings& settings, ds::ui::TouchManager& tm);
+		DrawTouchView(SpriteEngine& e);
+		DrawTouchView(SpriteEngine& e, cfg::Settings& settings, TouchManager& tm);
 
-		virtual void touchBegin(const ds::ui::TouchInfo& ti);
-		virtual void touchMoved(const ds::ui::TouchInfo& ti);
-		virtual void touchEnd(const ds::ui::TouchInfo& ti);
+		void touchBegin(const TouchInfo& ti) override;
+		void touchMoved(const TouchInfo& ti) override;
+		void touchEnd(const TouchInfo& ti) override;
 
-		virtual void drawLocalServer();
+		void drawLocalClient() override;
+
+		void onBuildRenderBatch() override;
 
 	  private:
-		std::map<int, Circle*> mCircles;
-		float				   mCircleRadius;
-		bool				   mCircleFilled;
-		ci::ColorA			   mCircleColor;
+		struct Instance /* std140 */ {
+			ci::vec2	 position;
+			glm::float32 radius;
+			glm::int32_t id;
+
+			bool operator==(const Instance& rhs) const { return id == rhs.id; }
+			bool operator!=(const Instance& rhs) const { return id != rhs.id; }
+			bool operator<(const Instance& rhs) const { return id < rhs.id; }
+
+			bool operator==(const int& rhs) const { return id == rhs; }
+			bool operator!=(const int& rhs) const { return id != rhs; }
+			bool operator<(const int& rhs) const { return id < rhs; }
+		};
+
+		std::vector<Instance> mInstances;
+		ci::gl::VboRef		  mBuffer;
+		float				  mCircleRadius;
+		bool				  mCircleFilled;
+		ci::ColorA			  mCircleColor;
 	};
 
 }}	   // namespace ds::ui
