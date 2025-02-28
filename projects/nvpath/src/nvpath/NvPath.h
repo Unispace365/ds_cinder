5eba4f58f4d481f69500ba4e73f4d772e182887a
paulhoux <paulhoux@users.noreply.github.com>
2025-02-28 10:22:17 +0100

    Added matrix conversion functions to nvpath.

--- projects/nvpath/src/nvpath/NvPath.h
+++ projects/nvpath/src/nvpath/NvPath.h
@@ -1204,8 +1204,16 @@ inline glm::mat3x3 toMat3x3(const glm::mat3x2& m) {
 	return {m[0][0], m[0][1], 0, m[1][0], m[1][1], 0, m[2][0], m[2][1], 1};
 }
 
+inline glm::mat3x3 toMat3x3(const glm::mat4x4& m) {
+	return {m[0][0], m[0][1], m[0][3], m[1][0], m[1][1], m[1][3], m[3][0], m[3][1], m[3][3]};
+}
+
 inline glm::mat4x4 toMat4x4(const glm::mat3x2& m) {
 	return {m[0][0], m[0][1], 0, 0, m[1][0], m[1][1], 0, 0, 0, 0, 1, 0, m[2][0], m[2][1], 0, 1};
+}
+
+inline glm::mat4x4 toMat4x4(const glm::mat3x3& m) {
+	return {m[0][0], m[0][1], 0, m[0][2], m[1][0], m[1][1], 0, m[1][2], 0, 0, 1, 0, m[2][0], m[2][1], 0, m[2][2]};
 }
 
 } // namespace nvpath
