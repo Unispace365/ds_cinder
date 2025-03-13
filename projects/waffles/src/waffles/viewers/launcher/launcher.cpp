f4842ebea244988c0ef1fc128d1d3de449e721cd
Luke Purcell <luke.purcell@downstream.com>
2025-03-13 10:32:01 +0100

    Disable 'select' mode in launcher

--- projects/waffles/src/waffles/viewers/launcher/launcher.cpp
+++ projects/waffles/src/waffles/viewers/launcher/launcher.cpp
@@ -336,7 +336,7 @@ void Launcher::updateItem(ds::ui::SmartLayout* item) {
 
 	auto sidePanelScroll = mPrimaryLayout->getSprite<ds::ui::SmartScrollList>("side_panel_content");
 	if (sidePanelScroll) {
-		ContentUtils::configureListItem(mEngine, item, ci::vec2(sidePanelScroll->getWidth(), item->getHeight()), true /* TODO only if layout builder is active */);
+		ContentUtils::configureListItem(mEngine, item, ci::vec2(sidePanelScroll->getWidth(), item->getHeight()), false /* TODO only if layout builder is active */);
 		item->setSize(sidePanelScroll->getWidth(), item->getHeight());
 	}
 	setButtonCallbacks(item);
