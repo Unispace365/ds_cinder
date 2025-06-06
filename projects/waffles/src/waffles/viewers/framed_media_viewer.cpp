6c4cdcfacbf00cbfdb9377616d7bcb1e17125888
paulhoux <paulhoux@users.noreply.github.com>
2025-06-06 12:38:29 +0200

    Additional fix for RH-324: the volume and controls were misaligned.

--- projects/waffles/src/waffles/viewers/framed_media_viewer.cpp
+++ projects/waffles/src/waffles/viewers/framed_media_viewer.cpp
@@ -143,35 +143,35 @@ void FramedMediaViewer::onLayout() {
 		}
 	}
 	// handle the mediaIOnterface sizing
-	auto	 controllerHolder	= mRootLayout->getSprite<ds::ui::LayoutSprite>("controller_holder");
-	auto	 innerSidebar		= mRootLayout->getSprite("inner_sidebar");
+	//auto	 controllerHolder	= mRootLayout->getSprite<ds::ui::LayoutSprite>("controller_holder");
+	//auto	 innerSidebar		= mRootLayout->getSprite("inner_sidebar");
 	//auto	 ui_holder			= mRootLayout->getSprite("ui_holder");
-	auto	 spacing			= mEngine.getWafflesSettings().getFloat("ui:button_spacing", 0, 16);
+	//auto	 spacing			= mEngine.getWafflesSettings().getFloat("ui:button_spacing", 0, 16);
 	ci::vec2 interfaceCheckSize = getSize();
 	bool	 changed			= interfaceCheckSize != mInterfaceCheckSize;
-	if (controllerHolder /*&& ui_holder*/ && innerSidebar && mMediaInterface && changed) {
+	if (/*controllerHolder && ui_holder && innerSidebar &&*/ mMediaInterface && changed) {
 		mInterfaceCheckSize = interfaceCheckSize;
-		auto interfaceBox	= mMediaInterface->getChildBoundingBox();
-		auto interfacePos	= mMediaInterface->localToGlobal(ci::vec3(interfaceBox.getUpperLeft(), 0));
-		auto fullContWidth	= (innerSidebar->getGlobalPosition().x + innerSidebar->getWidth()) - interfacePos.x;
-		auto contWidth		= interfaceBox.getWidth() + innerSidebar->getWidth() + spacing * 1;
-		auto w				= getWidth() - (mLeftPad + mRightPad);
-		auto pdf			= dynamic_cast<ds::ui::PDFInterface*>(mMediaInterface);
-		if (!mIsDetached && getWidth() - (mLeftPad + mRightPad) < contWidth && getWidth() > 0 && !pdf) {
-			controllerHolder->setOverallAlignment(ds::ui::LayoutSprite::kRight);
-			controllerHolder->mLayoutFudge = ci::vec3(-(innerSidebar->getWidth() + spacing * 1), 0, 0);
+		//auto interfaceBox	= mMediaInterface->getChildBoundingBox();
+		//auto interfacePos	= mMediaInterface->localToGlobal(ci::vec3(interfaceBox.getUpperLeft(), 0));
+		//auto fullContWidth	= (innerSidebar->getGlobalPosition().x + innerSidebar->getWidth()) - interfacePos.x;
+		//auto contWidth		= interfaceBox.getWidth() + innerSidebar->getWidth() + spacing * 1;
+		//auto w				= getWidth() - (mLeftPad + mRightPad);
+		//auto pdf			= dynamic_cast<ds::ui::PDFInterface*>(mMediaInterface);
+		//if (!mIsDetached && getWidth() - (mLeftPad + mRightPad) < contWidth && getWidth() > 0 && !pdf) {
+			//controllerHolder->setOverallAlignment(ds::ui::LayoutSprite::kRight);
+			//controllerHolder->mLayoutFudge = ci::vec3(-(innerSidebar->getWidth() + spacing * 1), 0, 0);
 			//auto offset					   = contWidth - w;
 			//ui_holder->mLayoutFudge		   = ci::vec3(offset * 0.5, 0, 0);
-		} else if (!mIsDetached && contWidth > fullContWidth) {
+		//} else if (!mIsDetached && contWidth > fullContWidth) {
 
-			controllerHolder->setOverallAlignment(ds::ui::LayoutSprite::kMiddle);
-			controllerHolder->mLayoutFudge = ci::vec3(-spacing * 1.1, 0, 0);
+			//controllerHolder->setOverallAlignment(ds::ui::LayoutSprite::kMiddle);
+			//controllerHolder->mLayoutFudge = ci::vec3(-spacing * 1.1, 0, 0);
 
-		} else {
-			controllerHolder->setOverallAlignment(ds::ui::LayoutSprite::kMiddle);
-			controllerHolder->mLayoutFudge = ci::vec3(0, 0, 0);
+		//} else {
+			//controllerHolder->setOverallAlignment(ds::ui::LayoutSprite::kMiddle);
+			//controllerHolder->mLayoutFudge = ci::vec3(0, 0, 0);
 			//ui_holder->mLayoutFudge		   = ci::vec3(0, 0, 0);
-		}
+		//}
 	}
 
 	if (mRootLayout) {
