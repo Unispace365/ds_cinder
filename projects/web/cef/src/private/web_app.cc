26143956166d99a2d2193a7bb53ee204357c3c6a
Benjamin Massey <benjamin.w.massey@gmail.com>
2026-01-22 15:25:17 -0800

    ignore certificate errors flag to cef

--- projects/web/cef/src/private/web_app.cc
+++ projects/web/cef/src/private/web_app.cc
@@ -64,6 +64,7 @@ void WebApp::OnBeforeCommandLineProcessing(const CefString& process_type, CefRef
 	command_line->AppendSwitch("disable-chrome-login-prompt");
 	//command_line->AppendSwitch("shared-texture-enabled");
 	//command_line->AppendSwitchWithValue("off-screen-frame-rate", "60");
+	command_line->AppendSwitch("ignore-certificate-errors");
 	command_line->AppendSwitchWithValue(CefString("touch-optimized-ui"),CefString("enabled"));
 }
 
